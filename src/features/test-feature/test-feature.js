f1991b05fb68fcd6d01b0ad46382d1dd89988b28
GregoryChocolate <47292994+GregoryChocolate@users.noreply.github.com>
2020-10-30 12:11:38 +0200

    Added index.js file for test feature

--- src/features/test-feature/test-feature.js
+++ src/features/test-feature/test-feature.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector, shallowEqual } from 'react-redux';
 
 import { addOne, subtractOne } from './redux/test-actions';
 import { counterSelector } from './redux/test-selectors';
 
 export const TestComponent = () => {
-  const counter = useSelector(counterSelector);
+  const counter = useSelector(counterSelector, shallowEqual);
   const dispatch = useDispatch();
 
   return (
