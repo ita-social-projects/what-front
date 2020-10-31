53475f105154d4732c3fdae88fdc8d433e8e5611
Mozdolevskyi Maksym <72501713+MozdolevskyiMaksym@users.noreply.github.com>
2020-10-31 18:07:34 +0300

    Create a webpack skeleton for React APP

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const App = () => {
+  return (
+    <h1>Hello!</h1>
+  );
+};
+ReactDom.render(<App />, document.getElementById('root'))
