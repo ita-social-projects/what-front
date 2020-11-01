d71c4a91c05d0a98f7c8d731585226a658469d16
MACHINE-EKT7PES\itcube <uaklopov@gmail.com>
2020-11-01 21:34:10 +0200

    added: style-loader, css-loader, postcss-loader, sass-loader

--- src/features/counter/counter.js
+++ src/features/counter/counter.js
@@ -3,7 +3,7 @@ import { useSelector, shallowEqual } from 'react-redux';
 
 import { counterSelector, counterActions } from './redux';
 import { useActions } from "../../shared/hooks";
-
+import styles from "./Button.css";
 
 export const Counter = () => {
   const counter = useSelector(counterSelector, shallowEqual);
@@ -12,8 +12,8 @@ export const Counter = () => {
   return (
     <div>
       <h2>Test counter: {counter}</h2>
-      <button onClick={incrementCounter}>Increment</button>
-      <button onClick={decrementCounter}>Decrement</button>
+      <button className={styles.baseButton} onClick={incrementCounter}>Increment</button>
+      <button className={styles.baseButton} onClick={decrementCounter}>Decrement</button>
     </div>
   );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
   mode: "development",
@@ -14,6 +15,40 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: ['babel-loader'],
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {loader: 'style-loader'},
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              modules: {
+                localIdentName: '[path]___[name]__[local]___[hash:base64:5]',
+              },
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    'autoprefixer',
+                  ],
+                ],
+                sourceMap: true
+              }
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          }
+        ],
       },
     ]
   },
