dc072943647909063d2e8e38e0227275d61c0b6b
Anastasia_G <anastasiagordienko99@gmail.com>
2020-11-05 11:25:29 +0200

    fix in import

--- src/app.js
+++ src/app.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 
 import { configureStore } from './store';
-import { Counter } from './features';
-import { Support } from './features';
+import { Counter, Support } from './features';
 
 const store = configureStore();
 
