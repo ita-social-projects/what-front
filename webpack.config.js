ec7c8d4ef0c181828d2436ecd075150fb86d5bf0
Oksana Fedotova <56606870+Winchestery@users.noreply.github.com>
2020-11-05 09:02:24 +0200

    Bootstrap added (#26)

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,14 @@ module.exports = {
         exclude: /node_modules/,
         use: ['babel-loader'],
       },
+      {
+        test: /\.scss$/,
+        use: [
+           "style-loader",
+            "css-loader",
+            "sass-loader"
+            ]
+        }
     ]
   },
   plugins: [
