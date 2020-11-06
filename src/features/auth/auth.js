33c9f7531f8686b2b4cd4041fdfe37ff54e509d9
Drozdov Oleg <drozdov.oleg87@gmail.com>
2020-11-06 14:16:20 +0200

    fix: styles modules

--- src/features/auth/auth.js
+++ src/features/auth/auth.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import styles from './auth-module.scss';
+import styles from './auth.module.scss';
 
 export const Auth = () => {  
   return (
-    <div className="wrapper">    
+    <div className={styles.wrapper}>    
       <div className="container">
         <div className="row justify-content-center">
           <div className="col-lg-4 col-md-6 col-sm-8">            
-            <div className="auth-container rounded">
-              <form className="auth-form p-3">
+            <div className={styles.form}>
+              <form className="p-3">
                 <div className="form-group">
                   <input type="email" className="form-control" id="auth-form__email" placeholder="Email address" />
                 </div>
@@ -17,7 +17,7 @@ export const Auth = () => {
                 </div>
                 <button type="submit" className="btn btn-block btn-warning">Sign in</button>
                 <div className="text-center mt-3">
-                  <p>Don't have an account? <a href="#">Registration</a></p>                  
+                  <p>Don't have an account? <a href="#" className={styles.link}>Registration</a></p>                  
                 </div>
               </form>
             </div>
