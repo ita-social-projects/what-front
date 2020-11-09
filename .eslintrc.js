cedeb010d56af8658e7b1be9f9aa591514ddb610
Pavel Kundenko <47292994+PavelKundenko@users.noreply.github.com>
2020-11-09 19:30:43 +0200

    Created modal window (#47)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,40 @@
 module.exports = {
-    env: {
-        browser: true,
-        commonjs: true,
-        es2021: true
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'airbnb',
+    'airbnb/hooks',
+  ],
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: [
-        'plugin:react/recommended',
-        'plugin:react-hooks/recommended',
-        'airbnb',
-        'airbnb/hooks',
-    ],
-    parser: '@babel/eslint-parser',
-    parserOptions: {
-        sourceType: 'module',
-        ecmaFeatures: {
-            jsx: true
-        },
-        ecmaVersion: 12
-    },
-    plugins: [
-        'babel',
-        'react',
-        'react-hooks'
-    ],
-    rules: {
-        semi: ['error', 'always'],
-        quotes: ['error', 'single'],
-        'linebreak-style': 0,
-        'eol-last': 0,
-        'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0 }],
-        'import/prefer-default-export': 'off',
-        'import/extensions': ['error', 'ignorePackages', {
-            js: 'always',
-        }],
-        'object-curly-spacing': [2, 'always'],
-        'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }]
-    },
+    ecmaVersion: 12,
+  },
+  plugins: [
+    'babel',
+    'react',
+    'react-hooks',
+  ],
+  rules: {
+    semi: ['error', 'always'],
+    quotes: ['error', 'single'],
+    'linebreak-style': 0,
+    'eol-last': 0,
+    'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0 }],
+    'import/prefer-default-export': 'off',
+    'import/extensions': ['error', 'ignorePackages', {
+      js: 'always',
+    }],
+    'object-curly-spacing': [2, 'always'],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/jsx-one-expression-per-line': 0,
+  },
 };
--- src/features/counter/counter.js
+++ src/features/counter/counter.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector, shallowEqual } from 'react-redux';
 
-import { counterSelector, counterActions } from './redux';
-import { useActions } from "../../shared/hooks";
-
+import { counterSelector, counterActions } from './redux/index.js';
+import { useActions } from '../../shared/hooks/index.js';
+import { ModalWindow } from '../modal-window/index.js';
 
 export const Counter = () => {
   const counter = useSelector(counterSelector, shallowEqual);
   const { incrementCounter, decrementCounter } = useActions(counterActions);
 
+  const [toShowModal, setShowModal] = useState(false);
+
+  const showModalHandler = () => setShowModal(true);
+
+  const closeModalHandler = () => setShowModal(false);
+
+  const submitModalHandler = () => {
+    console.log('Modal submitted, something happened!');
+    closeModalHandler();
+  };
+
   return (
     <div>
-      <h2>Test counter: {counter}</h2>
-      <button onClick={incrementCounter}>Increment</button>
-      <button onClick={decrementCounter}>Decrement</button>
+      <h2>
+        Test counter: {counter}
+      </h2>
+      <button type="button" onClick={incrementCounter}>Increment</button>
+      <button type="button" onClick={decrementCounter}>Decrement</button>
+      <button type="button" onClick={showModalHandler}>Show modal</button>
+      <ModalWindow
+        toShow={toShowModal}
+        onSubmit={submitModalHandler}
+        onClose={closeModalHandler}
+      >
+        *some action that you should confirm (or not confirm)*
+      </ModalWindow>
     </div>
   );
 };
