fc720e2961a09cf735709aa366ee124b38c2c43e
Drozdov Oleg <drozdov.oleg87@gmail.com>
2020-11-12 18:29:10 +0200

    added edit-students-details component layout

---
+++ src/features/edit-students-details/edit-students-details.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import styles from './edit-students-details.scss';
+import Icon from '../../icon';
+
+export const EditStudentsDetails = () => {
+  const student = {
+    firstName: 'Taras',
+    secondName: 'Tarasov',
+    email: 'tarastarasov.@gmail.com',
+    groups: [
+      { id: 1, name: 'Group 1' },
+      { id: 2, name: 'Group 2' },
+      { id: 3, name: 'Group 3' },
+      { id: 4, name: 'Group 4' },
+      { id: 5, name: 'Group 5' },
+      { id: 6, name: 'Group 6' }
+    ]
+  }
+
+  return (
+    <div className={styles.wrapper}>
+      <div className="container shadow pb-3">
+        {/* <h3>Edit student's details</h3> */}
+        {/* first name */}
+        <div className="row m-0 pt-3">
+          <div className="col-md-4">
+            <label htmlFor="first-name">First Name:</label>
+          </div>
+          <div className="col-md-8">
+            <input type="text" className={`${styles['input-style']} form-control`} id='first-name' placeholder={student.firstName} />
+          </div>
+        </div>
+        {/* second name */}
+        <div className="row m-0 pt-3">
+          <div className="col-md-4">
+            <label htmlFor="second-name">Second Name:</label>
+          </div>
+          <div className="col-md-8">
+            <input type="text" className={`${styles['input-style']} form-control`} id='second-name' placeholder={student.secondName} />
+          </div>
+        </div>
+        {/* email */}
+        <div className="row m-0 pt-3">
+          <div className="col-md-4">
+            <label htmlFor="email">Email:</label>
+          </div>
+          <div className="col-md-8">
+            <input type="text" className={`${styles['input-style']} form-control`} id='email' placeholder={student.email} />
+          </div>
+        </div>
+        {/* add group */}
+        <div className="row m-0 pt-3">
+          <div className="col-md-4">
+            <label htmlFor="groups">Group('s):</label>
+          </div>
+          <div className="col-md-8">
+            <div className="input-group flex-nowrap">
+              <input className={`${styles['input-style']} form-control`} list="group-list" placeholder="Type name of group" />              
+              <datalist id="group-list">                
+                {student.groups.map(({ id, name }) => <option key={id}>{name}</option>)}
+              </datalist>
+              <div className="input-group-append">
+                <button className="btn btn-warning" type="button"><Icon icon="Plus" /></button>
+              </div>
+            </div>
+          </div>          
+        </div>
+        {/* groups */}
+        <div className="row m-0 pt-3">
+          <div className="col-md-8 offset-md-4">
+            <ul className="d-flex flex-wrap justify-content-between p-0">
+              {student.groups.map(({ id, name }) => <li className={`${styles['list-element']} d-flex bg-light border border-outline-secondary rounded`} key={id}>{name}<button className="btn p-0 ml-auto mr-2 font-weight-bold text-danger">X</button></li>)}
+            </ul>
+          </div>
+        </div>
+        {/* buttons */}
+        <div className='row m-0 pt-3'>
+          <div className="col-md-3 col-4">
+            <div className={`${styles.button} btn btn-danger w-100`}>Exclude</div>
+          </div>
+          <div className="col-md-3 offset-md-3 col-4">
+            <div className={`${styles.button} btn btn-secondary w-100`}>Clear</div>
+          </div>
+          <div className="col-md-3 col-4">
+            <div className={`${styles.button} btn btn-success w-100`}>Save</div>
+          </div>
+        </div>
+      </div>
+      {/* <Icon icon="Plus" /> */}
+    </div>
+  )
+}
