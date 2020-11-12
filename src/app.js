d73433bcb9a8798a1e865833b6dbf11b7514ba81
maxim mozdolevskyi <mexicko@gmail.com>
2020-11-12 21:18:56 +0200

    Tabs component is done

--- src/app.js
+++ src/app.js
@@ -4,26 +4,15 @@ import { Provider } from 'react-redux';
 import { configureStore } from './store.js';
 import { Counter } from './features/index.js';
 import Icon from './icon.js';
-import { Tabs } from './components';
 
 const store = configureStore();
-const tabs = [
-  {id: 0, title: 'Mentor`s details', active: true, link: 'students'},
-  {id: 1, title: 'List of mentors', active: false, link: 'mentors'},
-  {id: 2, title: 'Schedule', active: false, link: 'schedule'},
-];
-
-const back = 'back'
 
 export const App = () => (
   
   <Provider store={store}>
-    {/*<h1>Hello, I'm App Component!</h1>
+    <h1>Hello, I'm App Component!</h1>
     <Counter />
     <Icon icon="Plus" size={32} color="#f78259" /> 
-<Icon icon="Edit"/>*/}
-    <div className='nav nav-tabs w-50 ml-3'>
-      <Tabs initialTabs={tabs} back={back}/>
-    </div>
+    <Icon icon="Edit"/>
   </Provider>
 );
--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -1,21 +1,21 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import propTypes from 'prop-types';
 import classNames from 'classnames';
 
 import styles from './Tabs.scss';
 
 const arrow = (
-  <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-arrow-left" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-    <path fillRule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+  <svg width='1em' height='1em' viewBox='0 0 16 16' className='bi bi-arrow-left' fill='currentColor' xmlns='http://www.w3.org/2000/svg'>
+    <path fillRule='evenodd' d='M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z'/>
   </svg>
 );
 
-
-export const Tabs = ({initialTabs, back}) => {
+export const Tabs = ({initialTabs, linkBack}) => {
   
   const [tabs, setTabs] = useState(initialTabs);
 
   const toggleActive = (event) => {
+
     setTabs((prevstate) => {
       prevstate.find((tab) => {
         if(tab.active) {
@@ -27,8 +27,8 @@ export const Tabs = ({initialTabs, back}) => {
         if(index == event.target.dataset.id) {
           return {
             ...tab,
-            active: !tab.active
-          } 
+            active: !tab.active,
+          }
         } else {
           return tab
         }
@@ -38,8 +38,8 @@ export const Tabs = ({initialTabs, back}) => {
 
   return (
     <>
-      <a className={classNames('nav-item nav-link', styles.back)} 
-        href={`#${back}`} 
+      <a className='nav-item nav-link d-flex align-items-center'
+        href={`#${linkBack}`} 
         onClick={toggleActive}
       >{arrow}</a>
       {tabs.map(tab => {
@@ -62,5 +62,6 @@ Tabs.propTypes = {
     active: propTypes.bool,
     link: propTypes.string.isRequired
   })),
-  back: propTypes.string.isRequired
+  linkBack: propTypes.string.isRequired
 }
+
