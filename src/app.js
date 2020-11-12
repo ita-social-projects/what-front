f6958208b76ff740373b856e1e502968da188dcb
maxim mozdolevskyi <mexicko@gmail.com>
2020-11-12 16:26:36 +0200

    Added Tab with icon(back), fixed styles

--- src/app.js
+++ src/app.js
@@ -9,20 +9,22 @@ import { Tabs } from './components';
 
 const store = configureStore();
 const tabs = [
-  {id: 0, title: 'Mentor`s details', active: false, link: 'students'},
+  {id: 0, title: 'Mentor`s details', active: true, link: 'students'},
   {id: 1, title: 'List of mentors', active: false, link: 'mentors'},
-  {id: 2, title: 'das', active: false, link: 'mentors'},
+  {id: 2, title: 'Schedule', active: false, link: 'schedule'},
 ];
+
+const back = 'back'
 
 export const App = () => (
   
     <Provider store={store}>
-      <h1>Hello, I'm App Component!</h1>
+      {/*<h1>Hello, I'm App Component!</h1>
       <Counter />
       <Icon icon="Plus" size={32} color="#f78259" /> 
-      <Icon icon="Edit"/>
+<Icon icon="Edit"/>*/}
       <div className='nav nav-tabs w-50'>
-        <Tabs tabs={tabs}/>
+        <Tabs initialTabs={tabs} back={back}/>
       </div>
     </Provider>
 );
--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -2,8 +2,16 @@ import React, {useState} from 'react';
 import propTypes from 'prop-types';
 import styles from './Tabs.scss';
 
-export const Tabs = ({tabs}) => {
-  const [tabss, setTabs] = useState(tabs)
+const arrow = (
+  <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-arrow-left" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    <path fillRule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+  </svg>
+);
+
+
+export const Tabs = ({initialTabs, back}) => {
+  
+  const [tabs, setTabs] = useState(initialTabs);
 
   const toggleActive = (event) => {
     setTabs((prevstate) => {
@@ -28,14 +36,18 @@ export const Tabs = ({tabs}) => {
   };
 
   return (
-      tabss.map(tab => {
-        return <a className={`${styles['tab']} nav-item nav-link ${tab.active ? styles['active'] : ''}`}
-          href={`#${tab.link}`}
-          key={tab.id}
-          data-id={tab.id} 
-          onClick={toggleActive}
-          >{tab.title}</a>
-      })
+    <>
+      <a className={`nav-item nav-link ${styles.back}`} href={`#${back}`} onClick={toggleActive}>{arrow}</a>
+      {tabs.map(tab => {
+        return (
+        <a className={`${styles['tab']} nav-item nav-link ${tab.active ? styles['active'] : ''}`}
+        href={`#${tab.link}`}
+        key={tab.id}
+        data-id={tab.id} 
+        onClick={toggleActive}
+        >{tab.title}</a>)
+      })}
+    </>
   );
 }
 
@@ -45,5 +57,6 @@ Tabs.propTypes = {
     title: propTypes.string.isRequired,
     active: propTypes.bool,
     link: propTypes.string.isRequired
-  }))
+  })),
+  back: propTypes.string.isRequired
 }
