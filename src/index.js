d29dfffd702f23f3105976849d775cf0a3ee5dc6
MACHINE-EKT7PES\itcube <uaklopov@gmail.com>
2020-11-13 06:20:28 +0200

    .

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './app.js';
 import './index.scss'
-
-
 
 ReactDOM.render(<App />, document.getElementById('root'));
