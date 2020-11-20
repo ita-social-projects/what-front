cee41a6ae9bf602ff941e982c67bd32d41bde6c2
Marina Pasternak <256pasternakmarina@gmail.com>
2020-11-20 14:06:30 +0200

    delete extra icons

--- src/app.js
+++ src/app.js
@@ -16,20 +16,5 @@ export const App = () => (
     <Counter />
     <Icon icon="Plus" size={32} className="icon" color="#FFB800" />
     <Icon icon="Edit" viewBox="0 0 50 50" color="#FFB800" />
-    <Icon icon="Back" color="#FFB800" />
-    <Icon icon="Calendar" color="#FFB800" />
-    <Icon icon="CheckboxFalse" color="#FFB800" />
-    <Icon icon="CheckboxNull" color="#FFB800" />
-    <Icon icon="CheckboxTrue" color="#FFB800" />
-    <Icon icon="CheckFalse" color="#FFB800" />
-    <Icon icon="CheckNull" color="#FFB800" />
-    <Icon icon="CheckTrue" color="#FFB800" />
-    <Icon icon="CloseButton" color="#FFB800" />
-    <Icon icon="DropDown" color="#FFB800" />
-    <Icon icon="DropDownButton" viewBox="0 0 50 50" color="#FFB800" />
-    <Icon icon="LogOut" viewBox="0 0 50 50" size={100} />
-    <Icon icon="Menu" viewBox="0 0 50 50" size={100} color="#FFB800" />
-    <Icon icon="Profile" viewBox="0 0 50 50" size={100} color="#FFB800" />
-    <Icon icon="Search" viewBox="0 0 50 50" size={100} color="#FFB800" />
   </Provider>
 );
