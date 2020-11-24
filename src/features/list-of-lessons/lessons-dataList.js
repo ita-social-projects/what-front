5098263cfb891d55f49a45d32449f3adb0527017
Oksana Fedotova <ksenkasloboda7@gmail.com>
2020-11-24 17:18:10 +0200

    fake datalist changed

--- src/features/list-of-lessons/lessons-dataList.js
+++ src/features/list-of-lessons/lessons-dataList.js
@@ -2,31 +2,76 @@ export const dataList = [
   {
     date: '2020-09-11',
     id: 1,
-    themeName: 'Python Core',
+    themeName: 'Learn JS',
   },
-
   {
     date: '2020-09-30',
     id: 2,
-    themeName: 'Py Core',
+    themeName: 'Advanced CSS',
   },
-
   {
     date: '2020-09-22',
     id: 3,
-    themeName: 'Pyre',
+    themeName: 'Basics of SCSS',
   },
-
   {
     date: '2020-09-10',
     id: 4,
-    themeName: 'Core',
+    themeName: 'REact Hooks',
   },
-
   {
     date: '2020-09-09',
     id: 5,
-    themeName: 'Python',
+    themeName: 'Redux',
   },
- 
+  {
+    date: '2020-09-09',
+    id: 6,
+    themeName: 'Form validation',
+  }, 
+  {
+    date: '2020-09-09',
+    id: 7,
+    themeName: 'HTML and CSS',
+  }, 
+  {
+    date: '2020-09-09',
+    id: 8,
+    themeName: 'Webpack',
+  }, 
+  {
+    date: '2020-09-09',
+    id: 10,
+    themeName: 'Thunk and Saga',
+  }, 
+  {
+    date: '2020-09-09',
+    id: 11,
+    themeName: 'Bootstrap',
+  },
+  {
+    date: '2020-09-09',
+    id: 12,
+    themeName: 'Tailwind CSS',
+  },
+  {
+    date: '2020-09-09',
+    id: 13,
+    themeName: 'Node JS',
+  },
+  {
+    date: '2020-09-09',
+    id: 14,
+    themeName: 'SVG graphic',
+  },
+  {
+    date: '2020-09-09',
+    id: 15,
+    themeName: 'Advanced HTML',
+  },
+  {
+    date: '2020-09-09',
+    id: 16,
+    themeName: 'Ajax requests',
+  },
 ];
