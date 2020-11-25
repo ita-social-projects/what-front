8c20ba7654d15d4954e27d874633b4814d6c11e0
Oksana Fedotova <ksenkasloboda7@gmail.com>
2020-11-25 14:14:31 +0200

    .:

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { configureStore } from './store.js';
-import { Counter } from './features/index.js';
+import { Counter, ListOfMentors } from './features/index.js';
 import Icon from './icon.js';
 
 const store = configureStore();
