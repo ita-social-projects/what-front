22fc3d189e8aa05350bb8306b1b4a3e62f86ad9e
Drozdov Oleg <drozdov.oleg87@gmail.com>
2020-11-27 19:15:34 +0200

    code refactoring

--- src/features/index.js
+++ src/features/index.js
@@ -7,7 +7,7 @@ export * from './auth';
 export * from './routes';
 export * from './header';
 export * from './edit-students-details';
-export * from './validation'
+export * from './validation';
 export * from './list-of-groups';
 export * from './list-of-students';
 export * from './list-of-courses';
