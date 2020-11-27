902ca4bfdf16635ee261a00654a725fff36c40dd
Drozdov Oleg <drozdov.oleg87@gmail.com>
2020-11-27 19:14:36 +0200

    added validation

--- src/features/edit-secretarys-details/edit-secretarys-details.js
+++ src/features/edit-secretarys-details/edit-secretarys-details.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import className from 'classnames';
 import { Formik, Form, Field } from 'formik';
-import { Button } from '../../components/button/index.js';
+import { Button } from '@components/button/index.js';
+import * as Yup from 'yup';
+import { validateEmail } from '@features/validation/validation-helpers.js';
+
 import styles from './edit-secretarys-details.scss';
 
 export const EditSecretarysDetails = () => {
@@ -11,45 +15,119 @@ export const EditSecretarysDetails = () => {
     lastName: 'Smith',
   };
 
+  const formValidate = Yup.object().shape({
+    firstName: Yup.string()
+      .matches('[^А-Яа-я]+', 'Invalid Input: only Latin letters allowed')
+      .matches('^[A-Z]', 'Invalid Input: capital letter needed')
+      .min(2, 'Too Short!')
+      .matches('^[A-Z]+[a-z]+$', 'Invalid Name')
+      .max(50, 'Too Long!')
+      .required('Name required'),
+    lastName: Yup.string()
+      .matches('[^А-Яа-я]+', 'Invalid Input: only Latin letters allowed')
+      .matches('^[A-Z]', 'Invalid Input: capital letter needed')
+      .min(2, 'Too Short!')
+      .matches('^[A-Z]+[a-z]+$', 'Invalid Last Name')
+      .max(50, 'Too Long!')
+      .required('Last Name required'),
+    // email: Yup.string()
+    //   .matches('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$',
+    //     'Invalid Email')
+    //   .required('Email required'),
+  });
+
   return (
     <div className={styles.wrapper}>
-      <div className="container rounded shadow pb-3">
-        <div className="row m-0 pt-3">
-          <div className="col-md-4">
-            <label htmlFor="first-name">First Name:</label>
-          </div>
-          <div className="col-md-8">
-            <input type="text" className="form-control" id="first-name" placeholder={secretary.firstName} />
-          </div>
-        </div>
-        <div className="row m-0 pt-3">
-          <div className="col-md-4">
-            <label htmlFor="second-name">Last Name:</label>
-          </div>
-          <div className="col-md-8">
-            <input type="text" className="form-control" id="second-name" placeholder={secretary.lastName} />
-          </div>
-        </div>
-        <div className="row m-0 pt-3">
-          <div className="col-md-4">
-            <label htmlFor="email">Email:</label>
-          </div>
-          <div className="col-md-8">
-            <input type="text" className="form-control" id="email" placeholder={secretary.email} />
-          </div>
-        </div>
-        <div className="row m-0 pt-3">
-          <div className="col-md-3 col-4">
-            <Button className="w-100" variant="danger">Fire</Button>
-          </div>
-          <div className="col-md-3 offset-md-3 col-4">
-            <Button className="w-100">Cancel</Button>
-          </div>
-          <div className="col-md-3 col-4">
-            <Button className="w-100" variant="success">Save</Button>
-          </div>
-        </div>
-      </div>
+      <Formik
+        initialValues={{
+          firstName: secretary.firstName,
+          lastName: secretary.lastName,
+          email: secretary.email,
+        }}
+        validationSchema={formValidate}
+        onSubmit={(values) => console.log(values)}
+      >
+        {({
+          values, errors, touched, handleReset, isValid, dirty,
+        }) => (
+          <Form>
+            <div className="container rounded shadow pb-3">
+              <div className="row m-0 pt-3">
+                <div className="col-md-4">
+                  <label htmlFor="firstName">First Name:</label>
+                </div>
+                <div className="col-md-8">
+                  <Field
+                    type="text"
+                    className={className('form-control', { 'border-danger': errors.firstName })}
+                    name="firstName"
+                    id="firstName"
+                    placeholder="Name"
+                    value={values.firstName}
+                  />
+                  {touched.firstName && errors.firstName && <div className="text-danger mt-3">{errors.firstName}</div>}
+                </div>
+              </div>
+              <div className="row m-0 pt-3">
+                <div className="col-md-4">
+                  <label htmlFor="lastName">Last Name:</label>
+                </div>
+                <div className="col-md-8">
+                  <Field
+                    type="text"
+                    className={className('form-control', { 'border-danger': errors.lastName })}
+                    name="lastName"
+                    id="lastName"
+                    placeholder="Lastname"
+                    value={values.lastName}
+                  />
+                  {touched.lastName && errors.lastName && <div className="text-danger mt-3">{errors.lastName}</div>}
+                </div>
+              </div>
+              <div className="row m-0 pt-3">
+                <div className="col-md-4">
+                  <label htmlFor="email">Email:</label>
+                </div>
+                <div className="col-md-8">
+                  <Field
+                    type="email"
+                    className={className('form-control', { 'border-danger': errors.email })}
+                    name="email"
+                    id="email"
+                    placeholder="Email"
+                    validate={validateEmail}
+                    value={values.email}
+                  />
+                  {touched.email && errors.email && <div className="text-danger mt-3">{errors.email}</div>}
+                </div>
+              </div>
+              <div className="row m-0 pt-3">
+                <div className="col-md-3 col-4">
+                  <Button className="w-100" variant="danger" onClick={() => { console.log('You are Fired!'); }}>Fire</Button>
+                </div>
+                <div className="col-md-3 offset-md-3 col-4">
+                  <button
+                    type="button"
+                    className={className(styles.button, 'btn btn-secondary w-100')}
+                    onClick={handleReset}
+                  >
+                    Cancel
+                  </button>
+                </div>
+                <div className="col-md-3 col-4">
+                  <button
+                    disabled={!isValid || !dirty}
+                    className={className(styles.button, 'btn btn-success w-100')}
+                    type="submit"
+                  >
+                    Save
+                  </button>
+                </div>
+              </div>
+            </div>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 };
