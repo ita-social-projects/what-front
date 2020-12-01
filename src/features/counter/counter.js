358b56462d33b3d6dbbd9058f8180b3b5f2d13d5
Pavel Kundenko <47292994+PavelKundenko@users.noreply.github.com>
2020-12-01 12:31:58 +0200

    Add withloader component (#170)

--- src/features/counter/counter.js
+++ src/features/counter/counter.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import { useSelector, shallowEqual } from 'react-redux';
 
-import { counterSelector, counterActions } from './redux/index.js';
+import { counterSelector, counterIsLoadingSelector, counterActions } from './redux/index.js';
 import { useActions } from '../../shared/hooks/index.js';
 import { ModalWindow } from '../modal-window/index.js';
-import { Button, Search } from '../../components/index.js';
+import { Button, Search, WithLoading } from '../../components/index.js';
 
 export const Counter = () => {
   const counter = useSelector(counterSelector, shallowEqual);
+  const isLoading = useSelector(counterIsLoadingSelector, shallowEqual);
 
   const { incrementCounter, decrementCounter, fetchCounter } = useActions(counterActions);
   const [toShowModal, setShowModal] = useState(false);
@@ -26,29 +27,31 @@ export const Counter = () => {
   };
 
   return (
-    <div>
+    <WithLoading isLoading={isLoading}>
       <div>
-        <h2>
-          Test counter: {counter}
-        </h2>
-        <Button type="button" onClick={incrementCounter}>Increment</Button>
-        <Button type="button" onClick={decrementCounter} variant="warning">Decrement</Button>
-        <Button type="button" onClick={handleShowModal} variant="success">Show modal</Button>
-        <Button onClick={fetchCounter} variant="primary">Fetch counter</Button>
+        <div>
+          <h2>
+            Test counter: {counter}
+          </h2>
+          <Button type="button" onClick={incrementCounter}>Increment</Button>
+          <Button type="button" onClick={decrementCounter} variant="warning">Decrement</Button>
+          <Button type="button" onClick={handleShowModal} variant="success">Show modal</Button>
+          <Button onClick={fetchCounter} variant="primary">Fetch counter</Button>
+        </div>
+        <div className="m-3">
+          <Search
+            onSearch={handleSearch}
+            placeholder="Search something"
+          />
+        </div>
+        <ModalWindow
+          toShow={toShowModal}
+          onSubmit={handleSubmitModal}
+          onClose={handleCloseModal}
+        >
+          *some action that you should confirm (or not confirm)*
+        </ModalWindow>
       </div>
-      <div className="m-3">
-        <Search
-          onSearch={handleSearch}
-          placeholder="Search something"
-        />
-      </div>
-      <ModalWindow
-        toShow={toShowModal}
-        onSubmit={handleSubmitModal}
-        onClose={handleCloseModal}
-      >
-        *some action that you should confirm (or not confirm)*
-      </ModalWindow>
-    </div>
+    </WithLoading>
   );
 };
