4ea2fba63bee68879cd0c2d72b867812cae508c6
Drozdov Oleg <drozdov.oleg87@gmail.com>
2020-12-01 22:42:12 +0200

    conected secretaries model to root-saga and rootReducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,9 +1,13 @@
 import { combineReducers } from 'redux';
 import { counterReducer, listOfGroupsReducer } from './features/index.js';
+import { secretariesReducer } from './models/index.js';
 
 export const rootReducer = combineReducers({
   features: combineReducers({
     counter: counterReducer,
     listOfGroups: listOfGroupsReducer,
+  }),
+  models: combineReducers({
+    secretaries: secretariesReducer,
   }),
 });
