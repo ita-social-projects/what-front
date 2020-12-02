8edefe5dcddb33e7391be2472cc45c191d20bb17
maxim mozdolevskyi <mexicko@gmail.com>
2020-12-02 21:42:33 +0200

    changed implementations of methods for changing our state

--- src/models/courses/reducer.js
+++ src/models/courses/reducer.js
@@ -1,7 +1,4 @@
-import { CREATING_COURSE_FAILED, CREATING_COURSE_STARTED, CREATING_COURSE_SUCCESS, 
-  EDITING_COURSE_FAILED, EDITING_COURSE_STARTED, EDITING_COURSE_SUCCESS, 
-   LOADING_COURSES_FAILED, LOADING_COURSES_STARTED, LOADING_COURSES_SUCCESS, 
-} from './types';
+import * as actionTypes from './types';
 
 const initialState = {
   data: null,
@@ -12,13 +9,13 @@ const initialState = {
 
 export const coursesReducer = (state = initialState, action) => {
   switch (action.type) {
-    case LOADING_COURSES_STARTED:
+    case actionTypes.LOADING_COURSES_STARTED:
       return {
         ...state,
         isLoading: true,
         error: '',
       };
-    case LOADING_COURSES_SUCCESS:
+    case actionTypes.LOADING_COURSES_SUCCESS:
       return {
         ...state,
         isLoading: false,
@@ -26,20 +23,20 @@ export const coursesReducer = (state = initialState, action) => {
         data: action.payload.courses,
         error: '',
       };
-    case LOADING_COURSES_FAILED:
+    case actionTypes.LOADING_COURSES_FAILED:
       return {
         ...state,
         isLoading: false,
         loaded: false,
         error: action.payload.error,
       };
-      case CREATING_COURSE_STARTED:
+      case actionTypes.CREATING_COURSE_STARTED:
         return {
           ...state,
           isLoading: true,
           error: '',
         };
-      case CREATING_COURSE_SUCCESS: 
+      case actionTypes.CREATING_COURSE_SUCCESS: 
         return {
           ...state,
           isLoading: false,
@@ -47,28 +44,29 @@ export const coursesReducer = (state = initialState, action) => {
           data: state.data.concat([action.payload.course]),
           error: '',
         };
-      case CREATING_COURSE_FAILED:
+      case actionTypes.CREATING_COURSE_FAILED:
         return {
           ...state,
           isLoading: false,
           loaded: false,
           error: action.payload.error,
         };
-      case EDITING_COURSE_STARTED:
+      case actionTypes.EDITING_COURSE_STARTED:
         return {
           ...state,
           isLoading: true,
           error: '',
         };
-      case EDITING_COURSE_SUCCESS:
+      case actionTypes.EDITING_COURSE_SUCCESS:
         return {
           ...state,
           isLoading: false,
           loaded: true,
-          data: action.payload.courses,
+          data: state.data.map((course) => (course.id === action.payload.course.id
+            ? action.payload.course : course)),
           error: '',
         };
-      case EDITING_COURSE_FAILED:
+      case actionTypes.EDITING_COURSE_FAILED:
         return {
           ...state,
           isLoading: false,
--- src/models/courses/sagas.js
+++ src/models/courses/sagas.js
@@ -1,20 +1,17 @@
 import { all, fork, put, call, takeLatest, select } from 'redux-saga/effects';
 import { ApiService } from '../api-service';
 import { coursesDataSelector } from './selectors';
-import { FETCH_COURSES, LOADING_COURSES_STARTED, LOADING_COURSES_SUCCESS, LOADING_COURSES_FAILED,
-  CREATE_COURSE, CREATING_COURSE_STARTED, CREATING_COURSE_SUCCESS, CREATING_COURSE_FAILED,
-  EDIT_COURSE, EDITING_COURSE_STARTED, EDITING_COURSE_SUCCESS, EDITING_COURSE_FAILED
-} from './types';
+import * as actionTypes from './types';
 
 export const getCourses = () => {
   return {
-    type: FETCH_COURSES,
+    type: actionTypes.FETCH_COURSES,
   };
 };
 
 export const createCourse = (course) => {
   return {
-    type: CREATE_COURSE,
+    type: actionTypes.CREATE_COURSE,
     payload: {
       course,
     },
@@ -23,7 +20,7 @@ export const createCourse = (course) => {
 
 export const editCourse = (course, id) => {
   return {
-    type: EDIT_COURSE,
+    type: actionTypes.EDIT_COURSE,
     payload: {
       course,
       id,
@@ -32,51 +29,44 @@ export const editCourse = (course, id) => {
 };
 
 export function* loadCoursesWatcher() {
-  yield takeLatest(FETCH_COURSES, loadCoursesWorker);
+  yield takeLatest(actionTypes.FETCH_COURSES, loadCoursesWorker);
 }
 
 export function* createCourseWatcher() {
-  yield takeLatest(CREATE_COURSE, createCourseWorker);
+  yield takeLatest(actionTypes.CREATE_COURSE, createCourseWorker);
 }
 
 export function* editCourseWatcher() {
-  yield takeLatest(EDIT_COURSE, editCourseWorker);
+  yield takeLatest(actionTypes.EDIT_COURSE, editCourseWorker);
 }
 
 function* loadCoursesWorker() {
   try {
-    yield put({type: LOADING_COURSES_STARTED});
+    yield put({type: actionTypes.LOADING_COURSES_STARTED});
     const courses = yield call(ApiService.load, '/courses');
-    yield put({type: LOADING_COURSES_SUCCESS, payload: {courses,}});
+    yield put({type: actionTypes.LOADING_COURSES_SUCCESS, payload: {courses}});
   } catch (error) {
-    yield put({type: LOADING_COURSES_FAILED, payload: {error: error.message}});
+    yield put({type: actionTypes.LOADING_COURSES_FAILED, payload: {error: error.message}});
   }
 }
 
 function* createCourseWorker(data) {
   try {
-    yield put({type: CREATING_COURSE_STARTED});
+    yield put({type: actionTypes.CREATING_COURSE_STARTED});
     const course = yield call(ApiService.create, '/courses', data.payload.course);
-    yield put({type: CREATING_COURSE_SUCCESS, payload: {course,}});
+    yield put({type: actionTypes.CREATING_COURSE_SUCCESS, payload: {course}});
   } catch (error) {
-    yield put({type: CREATING_COURSE_FAILED, payload: {error: error.message}});
+    yield put({type: actionTypes.CREATING_COURSE_FAILED, payload: {error: error.message}});
   }
 }
 
 function* editCourseWorker(data) {
   try {
-    yield put({type: EDITING_COURSE_STARTED});
+    yield put({type: actionTypes.EDITING_COURSE_STARTED});
     const course = yield call(ApiService.update, `/courses/${data.payload.id}`, data.payload.course);
-    const courses = yield select(coursesDataSelector);
-    const updatedCourses = courses.map((item) => {
-      if(item.id === course.id) {
-        return course;
-      }
-      return item;
-    });
-    yield put({type: EDITING_COURSE_SUCCESS, payload: {courses: updatedCourses}});
+    yield put({type: actionTypes.EDITING_COURSE_SUCCESS, payload: {course}});
   } catch (error) {
-    yield put({type: EDITING_COURSE_FAILED, payload: {error: error.message}});
+    yield put({type: actionTypes.EDITING_COURSE_FAILED, payload: {error: error.message}});
   }
 }
 
