f6d407e906ab81b668ef9ad4a3860b002bf4290b
maxim mozdolevskyi <mexicko@gmail.com>
2020-12-02 15:17:52 +0200

    resolved conflicts

--- src/app.js
+++ src/app.js
@@ -2,10 +2,8 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { configureStore } from './store.js';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { configureStore } from './store.js';
 import { Routes } from './features/index.js';
 import Icon from './icon.js';
-
 
 const store = configureStore();
 
