fd1bf9a2e7ac7cd8c099d0cc32761879dda8f739
maxim mozdolevskyi <mexicko@gmail.com>
2020-12-04 14:37:19 +0200

    fixed

--- src/app.js
+++ src/app.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { configureStore } from './store.js';
-import { BrowserRouter as Router } from 'react-router-dom';
 import { Routes } from './features/index.js';
 import Icon from './icon.js';
 
