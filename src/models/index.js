a50f605ab18478b6283fcb227b0c741a0871de19
maxim mozdolevskyi <mexicko@gmail.com>
2020-12-04 15:18:40 +0200

    fixed

--- src/models/index.js
+++ src/models/index.js
@@ -2,6 +2,4 @@ export {
   mentorsReducer, loadMentors, loadMentorsWatcher, mentorsIsLoadingSelector, mentorsDataSelector,
 } from './mentors/index.js';
 
-export { themesReducer, themesWatcher, themesDataSelector, themesIsLoadingSelector,
-  fetchThemes, createTheme, editTheme, deleteTheme,
-} from './themes/index.js'
+export * from './themes/index.js'
