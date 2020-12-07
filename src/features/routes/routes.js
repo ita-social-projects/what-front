2013c561a60a30f998b8a49e2281473e188d7c83
maxim mozdolevskyi <mexicko@gmail.com>
2020-12-07 22:05:10 +0200

    added routes, fixed spaces in edit lesson component

--- src/features/routes/routes.js
+++ src/features/routes/routes.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { Counter, ListOfStudents, NotFound, ListOfCourses } from '../index.js';
-import { Links, RoleList, Role } from './helpers.js';
+import { Counter, ListOfStudents, NotFound, ListOfCourses, EditCourse } from '../index.js';
+import { RoleList, Role } from './helpers.js';
 
 export const Routes = () => (
   <>
-    <Links />
     <Switch>
       <Route exact path="/" render={() => (<h1 className="m-3">Welcome to the WHAT project!</h1>)} />
       <Route exact path="/counter" component={Counter} />
@@ -13,6 +12,7 @@ export const Routes = () => (
       <Route exact path="/role-list/:role" component={Role} />
       <Route exact path="/students" component={ListOfStudents} />
       <Route exact path='/courses' component={ListOfCourses}/>
+      <Route exact path='/courses/edit-course/:id' component={EditCourse} />
       <Route component={NotFound} />
     </Switch>
   </>
