88d030d152bec501848f6bf44956d5bd1a016c22
maxim mozdolevskyi <mexicko@gmail.com>
2020-12-07 13:12:41 +0200

    fixed selectors

--- src/models/themes/selectors.js
+++ src/models/themes/selectors.js
@@ -1,2 +1,1 @@
-export const themesDataSelector = (state) => state.models.themes.data;
-export const themesIsLoadingSelector = (state) => state.models.themes.isLoading;
+export const themesSelector = (state) => state.models.themesж
