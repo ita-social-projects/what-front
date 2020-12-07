019b2b2e06f6f2afbe4816cc296bae788cf81f9c
Drozdov Oleg <drozdov.oleg87@gmail.com>
2020-12-07 13:00:16 +0200

    fixed selector export

--- src/models/secretary/selectors.js
+++ src/models/secretary/selectors.js
@@ -1,3 +1,1 @@
-export const secretariesSelector = (state) => state.models.secretaries.data;
-export const secretariesIsLoading = (state) => state.models.secretaries.isLoading;
-export const secretariesLoaded = (state) => state.models.secretaries.loaded;
+export const secretariesSelector = (state) => state.models.secretaries;
