321f33f01bcbda3b9d4837fce406cabb70c65c8c
GregoryChocolate <47292994+GregoryChocolate@users.noreply.github.com>
2020-12-07 14:21:00 +0200

    fix: renamed main students watcher

--- src/root-saga.js
+++ src/root-saga.js
@@ -2,14 +2,14 @@ import { all, fork } from 'redux-saga/effects';
 
 import { watchFetchCounter } from './features/counter/redux/index.js';
 import {
-  loadMentorsWatcher, themesWatcher, coursesWatcher, schedulesWatcher, studentsSaga,
+  loadMentorsWatcher, themesWatcher, coursesWatcher, schedulesWatcher, studentsWatcher,
 } from './models/index.js';
 
 export function* rootSaga() {
   yield all([
     fork(watchFetchCounter),
     fork(coursesWatcher),
-    fork(studentsSaga),
+    fork(studentsWatcher),
     fork(loadMentorsWatcher),
     fork(schedulesWatcher),
     fork(themesWatcher),
