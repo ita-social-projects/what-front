bcc38ac0925da94e39c6345288507b1ce23faa9a
maxim mozdolevskyi <mexicko@gmail.com>
2020-12-07 13:18:25 +0200

    resolved conflicts

--- src/shared/index.js
+++ src/shared/index.js
@@ -1,6 +1,2 @@
 export { useActions } from './hooks/index.js';
-<<<<<<< HEAD
 export { ApiService } from './api-service/index.js';
-=======
-export { ApiService } from './api-service/index.js';
->>>>>>> dev
