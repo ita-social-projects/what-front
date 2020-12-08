5b11807833df40a234b27317b8068e6d37f85f84
Volodymyr Rusynov <vrusynov@gmail.com>
2020-12-08 11:15:37 +0200

    update export

--- src/models/student-groups/index.js
+++ src/models/student-groups/index.js
@@ -1,3 +1,9 @@
-export * from './actions';
+export {
+  globalLoadStudentGroups,
+  loadStudentGroupsById,
+  editStudentGroup,
+  addStudentGroup,
+  studentGroupsWatcher,
+} from './actions.js';
 export { studentGroupsReducer } from './reducer.js';
 export { studentGroupsSelector } from './selector.js';
