195685bd1c126789e9be749ee5c1674c811a7eab
Drozdov Oleg <drozdov.oleg87@gmail.com>
2020-12-10 18:49:52 +0200

    added secretarys details route

--- src/features/routes/routes.js
+++ src/features/routes/routes.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { Counter, ListOfStudents, NotFound, ListOfCourses, AddCourse, EditCourse } from '../index.js';
-import { RoleList, Role } from './helpers.js';
+import { SecretariesTabs } from '@/screens/index.js';
+import {
+  Counter, ListOfStudents, NotFound, ListOfCourses, AddCourse, EditCourse, ListOfSecretaries,
+} from '../index.js';
 
 export const Routes = () => (
   <>
     <Switch>
       <Route exact path="/" render={() => (<h1 className="m-3">Welcome to the WHAT project!</h1>)} />
       <Route exact path="/counter" component={Counter} />
-      <Route exact path="/role-list" component={RoleList} />
-      <Route exact path="/role-list/:role" component={Role} />
       <Route exact path="/students" component={ListOfStudents} />
-      <Route exact path='/courses' component={ListOfCourses}/>
-      <Route exact path='/courses/edit-course/:id' component={EditCourse} />
-      <Route exact path='/add-course' component={AddCourse} />
+      <Route exact path="/courses" component={ListOfCourses} />
+      <Route exact path="/courses/edit-course/:id" component={EditCourse} />
+      <Route exact path="/add-course" component={AddCourse} />
+      <Route exact path="/secretaries" component={ListOfSecretaries} />
+      <Route exact path="/secretaries/:id" render={() => <SecretariesTabs index={0} />} />
+      <Route exact path="/secretaries/edit/:id" render={() => <SecretariesTabs index={1} />} />
       <Route component={NotFound} />
     </Switch>
   </>
