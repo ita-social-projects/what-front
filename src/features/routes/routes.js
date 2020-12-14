f50583f314128742c19b038727472f6d047261eb
GregoryChocolate <47292994+GregoryChocolate@users.noreply.github.com>
2020-12-14 15:36:19 +0200

    fix: minor error

--- src/features/routes/routes.js
+++ src/features/routes/routes.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { Counter, ListOfStudents, NotFound, ListOfCourses, AddCourse, EditCourse } from '../index.js';
+
+import {
+  Counter, ListOfStudents, NotFound,
+  ListOfCourses, AddCourse, EditCourse,
+} from '../index.js';
 import { RoleList, Role } from './helpers.js';
 
 export const Routes = () => (
@@ -11,9 +15,9 @@ export const Routes = () => (
       <Route exact path="/role-list" component={RoleList} />
       <Route exact path="/role-list/:role" component={Role} />
       <Route exact path="/students" component={ListOfStudents} />
-      <Route exact path='/courses' component={ListOfCourses}/>
-      <Route exact path='/courses/edit-course/:id' component={EditCourse} />
-      <Route exact path='/add-course' component={AddCourse} />
+      <Route exact path="/courses" component={ListOfCourses} />
+      <Route exact path="/courses/edit-course/:id" component={EditCourse} />
+      <Route exact path="/add-course" component={AddCourse} />
       <Route component={NotFound} />
     </Switch>
   </>
