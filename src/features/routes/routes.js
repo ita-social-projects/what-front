3830547e37b8a1fe2614eab6ef4c8630994c6dc7
GregoryChocolate <47292994+GregoryChocolate@users.noreply.github.com>
2020-12-14 15:21:52 +0200

    fix: merge conflicts

--- src/features/routes/routes.js
+++ src/features/routes/routes.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
+import { CoursesTabs, GroupsTabs } from '@/screens/index.js';
 import {
   Counter, ListOfStudents, NotFound,
-  ListOfCourses, AddCourse, EditCourse,
-  ListOfGroups, Auth,
+  ListOfCourses, AddCourse, ListOfGroups,
+  Auth,
 } from '../index.js';
-import { CoursesTabs, GroupsTabs } from '@/screens/index.js';
 
 export const Routes = () => (
   <>
     <Switch>
       <Route exact path="/" render={() => (<h1 className="m-3">Welcome to the WHAT project!</h1>)} />
       <Route exact path="/counter" component={Counter} />
-      <Route exact path="/role-list" component={RoleList} />
-      <Route exact path="/role-list/:role" component={Role} />
       <Route exact path="/students" component={ListOfStudents} />
-      <Route exact path='/courses' component={ListOfCourses}/>
-      <Route exact path='/courses/add-course' component={AddCourse} />
-      <Route exact path='/courses/:id' component={() => <CoursesTabs index={0}/>} />
-      <Route exact path='/courses/edit/:id' component={() => <CoursesTabs index={1}/>} />
+      <Route exact path="/courses" component={ListOfCourses} />
+      <Route exact path="/courses/add-course" component={AddCourse} />
+      <Route exact path="/courses/:id" component={() => <CoursesTabs index={0} />} />
+      <Route exact path="/courses/edit/:id" component={() => <CoursesTabs index={1} />} />
       <Route exact path="/groups" component={ListOfGroups} />
       <Route exact path="/groups/:id" render={() => <GroupsTabs index={0} />} />
       <Route exact path="/groups/edit/:id" render={() => <GroupsTabs index={1} />} />
