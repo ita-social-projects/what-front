27b0d0b426fa29ae4c78b7ada8658965e78e46a2
Volodymyr Rusynov <vrusynov@gmail.com>
2020-12-14 15:22:56 +0200

    fix: split group reducer

--- src/models/student-groups/selector.js
+++ src/models/student-groups/selector.js
@@ -1,1 +1,7 @@
 export const studentGroupsSelector = (state) => state.models.studentGroups;
+export const addStudentGroupsSelector = (state) => state.models.studentGroups.addStudentGroups;
+export const editStudentGroupsSelector = (state) => state.models.studentGroups.editStudentGroups;
+export const loadStudentGroupsSelector = (state) => state.models.studentGroups.loadStudentGroups;
+export const loadStudentGroupsByIdSelector = (state) => (
+  state.models.studentGroups.loadStudentGroupsById
+);
