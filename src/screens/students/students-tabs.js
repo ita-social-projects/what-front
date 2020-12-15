cde4e79b8659cce68d3b179b2ada6bedc66e52dd
MozdolevskyiMaksym <mexickoe@gmail.com>
2020-12-15 18:52:01 +0200

    added loading courses and groups

--- src/screens/students/students-tabs.js
+++ src/screens/students/students-tabs.js
@@ -1,14 +1,14 @@
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useActions } from '@/shared';
-import { globalLoadStudentGroups, loadActiveStudents } from '@/models';
+import { globalLoadStudentGroups, loadActiveStudents, fetchCourses } from '@/models';
 import { Tab, Tabs } from '@/components';
 import { EditStudentsDetails, StudentDetails } from '@/features';
 
 export const StudentsTabs = ({index}) => {
   const { id } = useParams();
 
-  const [fetchStudents, fetchGroups] = useActions([loadActiveStudents, globalLoadStudentGroups]);
+  const [fetchStudents, fetchGroups, loadCourses] = useActions([loadActiveStudents, globalLoadStudentGroups, fetchCourses]);
 
   useEffect(() => {
     fetchStudents();
@@ -17,6 +17,10 @@ export const StudentsTabs = ({index}) => {
   useEffect(() => {
     fetchGroups();
   }, [fetchGroups]);
+
+  useEffect(() => {
+    loadCourses();
+  }, [loadCourses]);
 
   return (
     <Tabs defaultIndex={index} className='container w-50' linkBack='/students'>
