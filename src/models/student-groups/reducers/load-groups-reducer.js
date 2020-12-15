2e0d3d26e303e7f07131e7d50fafbd16c47352ae
Volodymyr Rusynov <vrusynov@gmail.com>
2020-12-15 15:25:14 +0200

    fix: load-group-reducer.js

--- src/models/student-groups/reducers/load-groups-reducer.js
+++ src/models/student-groups/reducers/load-groups-reducer.js
@@ -20,7 +20,7 @@ export const loadStudentGroupsReducer = (state = INITIAL_STATE, action) => {
         ...state,
         isLoading: false,
         isLoaded: true,
-        groups: [...state.groups, ...action.payload],
+        groups: action.payload,
       };
     case actionTypes.LOADING_STUDENT_GROUPS_FAILED:
       return {
