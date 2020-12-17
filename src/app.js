4d3327952f07acb1eb8e4aa49d611c04d4057076
Marina Pasternak <256pasternakmarina@gmail.com>
2020-12-18 00:48:43 +0300

    resolve conflicts

--- src/app.js
+++ src/app.js
@@ -2,13 +2,5 @@ import React from 'react';
 import { Routes } from '@/shared';
 
 export const App = () => (
-<<<<<<< HEAD
-  <>
-    <h1>Hello, I`m App Component!</h1>
-    <Icon icon="Plus" size={32} className="icon" color="#FFB800" />
-    <Icon icon="Edit" viewBox="0 0 50 50" color="#FFB800" />
-  </>
-=======
   <Routes />
->>>>>>> dev
 );
