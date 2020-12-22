f094a8f328b7e832a789c29ef2921597f9ea300c
Drozdov Oleg <drozdov.oleg87@gmail.com>
2020-12-22 19:12:57 +0200

    added validation on each form

--- src/features/validation/validation-helpers.js
+++ src/features/validation/validation-helpers.js
@@ -1,25 +1,156 @@
 import * as Yup from 'yup';
-import * as yup from 'yup';
+
+export const authValidation = Yup.object().shape({
+  email: Yup.string()
+    .required('This field is required'),
+  password: Yup.string()
+    .required('This field is required'),
+});
+
+export const registrationValidate = Yup.object().shape({
+  firstName: Yup.string()
+    .min(2, 'Too short')
+    .matches('^[A-Za-zа-яА-ЯёЁ]+$', 'Invalid first name')
+    .max(50, 'Too long')
+    .required('This field is required'),
+  lastName: Yup.string()
+    .min(2, 'Too short')
+    .matches('^[A-Za-zа-яА-ЯёЁ]+-?[A-Za-zа-яА-ЯёЁ]+$', 'Invalid second name')
+    .max(50, 'Too longs')
+    .required('This field is required'),
+  email: Yup.string()
+    .email('Invalid email address')
+    .required('This field is required'),
+  password: Yup.string()
+    .min(6, 'Password has to be longer than 6 characters')
+    .required('This field is required'),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref('password'), null], 'You should confirm your password')
+    .required('This field is required'),
+});
+
+export const addCourseValidation = Yup.object().shape({
+  name: Yup.string()
+    .min(2, 'Too short')
+    .matches('^[A-Za-zа-яА-ЯёЁ]+[:-_ ]?[A-Za-zа-яА-ЯёЁ]+$', 'Invalid course name')
+    .max(50, 'Too long')
+    .required('This field is required'),
+});
+
+export const editCourseValidation = Yup.object().shape({
+  name: Yup.string()
+    .min(2, 'Too short')
+    .matches('^[A-Za-zа-яА-ЯёЁ]+[:-_ ]?[A-Za-zа-яА-ЯёЁ]+$', 'Invalid course name')
+    .max(50, 'Too long')
+    .required('This field is required'),
+});
+
+export const editGroupValidation = Yup.object().shape({
+  name: Yup.string()
+    .min(2, 'Too short')
+    .matches('^[A-Za-zа-яА-ЯёЁ]+[:-_ ]?[A-Za-zа-яА-ЯёЁ]+$', 'Invalid course name')
+    .max(50, 'Too long')
+    .required('This field is required'),
+  startDate: Yup.date()
+    .required('This field is required'),
+  finishDate: Yup.date()
+    .min(Yup.ref('startDate'), 'Finish date can\'t be before start date')
+    .required('This field is required'),
+});
+
+export const addLessonValidation = Yup.object().shape({
+  themeName: Yup.string()
+    .min(2, 'Too short')
+    .matches('^[A-Za-zа-яА-ЯёЁ]+[:-_ ]?[A-Za-zа-яА-ЯёЁ]+$', 'Invalid theme name')
+    .max(50, 'Too long')
+    .required('This field is required'),
+  groupName: Yup.string()
+    .min(2, 'Too short')
+    .matches('^[A-Za-zа-яА-ЯёЁ]+[:-_ ]?[A-Za-zа-яА-ЯёЁ]+$', 'Invalid group name')
+    .max(50, 'Too long')
+    .required('This field is required'),
+  lessonDate: Yup.string()
+    .required('This field is required'),
+  mentorEmail: Yup.string()
+    .email('Invalid email address')
+    .required('This field is required'),
+});
+
+export const editSecretaryValidate = Yup.object().shape({
+  firstName: Yup.string()
+    .min(2, 'Too short')
+    .matches('^[A-Za-zа-яА-ЯёЁ]+$', 'Invalid first name')
+    .max(50, 'Too long')
+    .required('This field is required'),
+  lastName: Yup.string()
+    .min(2, 'Too short')
+    .matches('^[A-Za-zа-яА-ЯёЁ]+-?[A-Za-zа-яА-ЯёЁ]+$', 'Invalid second name')
+    .max(50, 'Too longs')
+    .required('This field is required'),
+  email: Yup.string()
+    .email('Invalid email address')
+    .required('This field is required'),
+});
+
+export const editMentorValidate = Yup.object().shape({
+  firstName: Yup.string()
+    .min(2, 'Too short')
+    .matches('^[A-Za-zа-яА-ЯёЁ]+$', 'Invalid first name')
+    .max(50, 'Too long')
+    .required('This field is required'),
+  lastName: Yup.string()
+    .min(2, 'Too short')
+    .matches('^[A-Za-zа-яА-ЯёЁ]+-?[A-Za-zа-яА-ЯёЁ]+$', 'Invalid second name')
+    .max(50, 'Too longs')
+    .required('This field is required'),
+  email: Yup.string()
+    .email('Invalid email address')
+    .required('This field is required'),
+});
+
+export const editStudentValidate = Yup.object().shape({
+  firstName: Yup.string()
+    .min(2, 'Too short')
+    .matches('^[A-Za-zа-яА-ЯёЁ]+$', 'Invalid first name')
+    .max(50, 'Too long')
+    .required('This field is required'),
+  lastName: Yup.string()
+    .min(2, 'Too short')
+    .matches('^[A-Za-zа-яА-ЯёЁ]+-?[A-Za-zа-яА-ЯёЁ]+$', 'Invalid second name')
+    .max(50, 'Too longs')
+    .required('This field is required'),
+  email: Yup.string()
+    .email('Invalid email address')
+    .required('This field is required'),
+});
 
 export const formValidate = Yup.object().shape({
   firstName: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
-    .matches(
-      '^[A-Za-zа-яА-ЯёЁ]+$',
-      'Invalid first name',
-    )
+    .min(2, 'Too short')
+    .matches('^[A-Za-zа-яА-ЯёЁ]+$', 'Invalid first name')
+    .max(50, 'Too long')
     .required('This field is required'),
   lastName: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
-    .matches(
-      '^[A-Za-zа-яА-ЯёЁ]+$',
-      'Invalid second name',
-    )
+    .min(2, 'Too short')
+    .matches('^[A-Za-zа-яА-ЯёЁ]+-?[A-Za-zа-яА-ЯёЁ]+$', 'Invalid second name')
+    .max(50, 'Too longs')
     .required('This field is required'),
   email: Yup.string()
-    .email('Invalid Email')
+    .email('Invalid email address')
+    .required('This field is required'),
+  password: Yup.string()
+    .min(6, 'Password has to be longer than 6 characters')
+    .required('This field is required'),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref('password'), null], 'You should confirm your password')
+    .required('This field is required'),
+  date: Yup.string()
+    .matches('^([0-3][0-9]).([0][1-9]|[1][0-2]).([12][0-9]{3})$', 'Invalid date')
+    .required('This field is required'),
+  name: Yup.string()
+    .min(2, 'Too short')
+    .matches('^[A-Za-zа-яА-ЯёЁ]+[-_ ]?[A-Za-zа-яА-ЯёЁ]+$', 'Invalid course name')
+    .max(50, 'Too long')
     .required('This field is required'),
 });
 
@@ -91,9 +222,9 @@ export const validateName = (value) => {
   return error;
 };
 
-export const authValidationSchema = yup.object().shape({
-  email: yup.string()
+export const authValidationSchema = Yup.object().shape({
+  email: Yup.string()
     .required('Email is required'),
-  password: yup.string()
+  password: Yup.string()
     .required('Password is required'),
 });
