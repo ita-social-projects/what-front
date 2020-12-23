c6fbc302211582e18ded389c5d2c04a526b032a6
Oksana Fedotova <56606870+Winchestery@users.noreply.github.com>
2020-12-23 09:44:10 +0200

    Proper header according to the specific role (#292)
    * initial commit
    
    * Header added
    
    * fix
    
    * fix links

--- src/features/lessons/add-lesson/add-lesson.js
+++ src/features/lessons/add-lesson/add-lesson.js
@@ -6,9 +6,9 @@ import {
 import classNames from 'classnames';
 import { useSelector, shallowEqual } from 'react-redux';
 import * as Yup from 'yup';
-
 import { Button } from '@/components';
 import { useActions, paths } from '@/shared';
+
 
 import {
   mentorsSelector,
