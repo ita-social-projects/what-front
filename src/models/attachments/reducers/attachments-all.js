93eca1aafcb0cef052a5bbf8e7182d5463cddd06
Oksana Fedotova <56606870+Winchestery@users.noreply.github.com>
2021-01-06 13:41:41 +0200

    Update attachments-all.js

--- src/models/attachments/reducers/attachments-all.js
+++ src/models/attachments/reducers/attachments-all.js
@@ -25,6 +25,14 @@ export const attachmentsAllReducer = (state = initialState, action) => {
         isLoaded: true,
       };
       
+   case types.FETCHING_ATTACHMENTS_FAILED:
+      return {
+       ...state,
+        isLoading: false,
+        isLoaded: false,
+        error: action.payload.error.message,
+      };
+      
     default:
       return state;
   }
