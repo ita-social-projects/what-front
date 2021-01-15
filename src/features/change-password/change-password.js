360ea2007585775d66be996352ca8e2f1464ce47
Drozdov Oleg <drozdov.oleg87@gmail.com>
2021-01-15 20:59:14 +0200

    added modal window, alerts and loader

--- src/features/change-password/change-password.js
+++ src/features/change-password/change-password.js
@@ -1,24 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { shallowEqual, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { paths, useActions } from '@/shared';
-import { currentUserSelector, newPassword } from '@/models';
+import { changePasswordSelector, currentUserSelector, newPassword } from '@/models';
+
+import { Button, WithLoading } from '@/components';
+import { addAlert, ModalWindow } from '@/features';
 
 import { Formik, Form, Field } from 'formik';
 import { changePasswordValidation } from '@features/validation/validation-helpers.js';
-import { Button } from '@/components';
 
 import className from 'classnames';
 import styles from './change-password.scss';
 
 export const ChangePassword = () => {
   const { currentUser } = useSelector(currentUserSelector, shallowEqual);
-  const [setNewPassword] = useActions([newPassword]);
+  const [password, setPassword] = useState({});
+
+  const {
+    isLoading: isChangePasswordLoading,
+    isLoaded: isChangePasswordLoaded,
+    error: changePasswordError,
+  } = useSelector(changePasswordSelector, shallowEqual);
+
+  const [setNewPassword, dispatchAddAlert] = useActions([newPassword, addAlert]);
 
   const history = useHistory();
 
+  const [toShowModal, setShowModal] = useState(false);
+
+  const handleShowModal = () => setShowModal(true);
+  const handleCloseModal = () => setShowModal(false);
+
+  useEffect(() => {
+    if (!changePasswordError && isChangePasswordLoaded) {
+      history.push(paths.MY_PROFILE);
+      dispatchAddAlert('The password has been successfully changed', 'success');
+    }
+    if (changePasswordError && !isChangePasswordLoaded) {
+      dispatchAddAlert(changePasswordError);
+    }
+  }, [changePasswordError, isChangePasswordLoaded, history, dispatchAddAlert]);
+
   const onSubmit = (value) => {
-    setNewPassword(value);
+    handleShowModal();
+    setPassword(value);
+  };
+
+  const handleConfirm = () => {
+    handleCloseModal();
+    setNewPassword(password);
   };
 
   return (
@@ -27,104 +58,115 @@ export const ChangePassword = () => {
         <div className="container pb-2">
           <h3 className="pt-3">Change Password</h3>
           <hr />
-          <Formik
-            initialValues={{
-              email: currentUser?.email,
-              currentPassword: '',
-              newPassword: '',
-              confirmNewPassword: '',
-            }}
-            validationSchema={changePasswordValidation}
-            onSubmit={onSubmit}
-          >
-            {({ values, errors, touched, isValid, dirty }) => (
-              <Form>
-                <div className="container px-0">
-                  <div className="row m-0">
-                    <div className="col-md-4 pt-1 px-0">
-                      <label htmlFor="email" className="font-weight-bold">Email address:</label>
+          <WithLoading isLoading={isChangePasswordLoading && !isChangePasswordLoaded} className="d-block mx-auto">
+            <Formik
+              initialValues={{
+                email: currentUser?.email,
+                currentPassword: '',
+                newPassword: '',
+                confirmNewPassword: '',
+              }}
+              validationSchema={changePasswordValidation}
+              onSubmit={onSubmit}
+            >
+              {({ values, errors, touched, isValid, dirty }) => (
+                <Form>
+                  <div className="container px-0">
+                    <div className="row m-0">
+                      <div className="col-md-4 pt-1 px-0">
+                        <label htmlFor="email" className="font-weight-bold">Email address:</label>
+                      </div>
+                      <div className="col-md-8 px-0">
+                        <Field
+                          type="email"
+                          className="form-control bg-white"
+                          name="email"
+                          id="email"
+                          value={values?.email}
+                          disabled
+                        />
+                      </div>
                     </div>
-                    <div className="col-md-8 px-0">
-                      <Field
-                        type="email"
-                        className="form-control bg-white"
-                        name="email"
-                        id="email"
-                        value={values?.email}
-                        disabled
-                      />
+                    <div className="row m-0 pt-3">
+                      <div className="col-md-4 pt-1 px-0">
+                        <label htmlFor="currentPassword" className="font-weight-bold">Current password:</label>
+                      </div>
+                      <div className="col-md-8 px-0">
+                        <Field
+                          type="password"
+                          className={className('form-control', { 'border-danger': errors.currentPassword })}
+                          name="currentPassword"
+                          id="currentPassword"
+                          placeholder="Current password"
+                        />
+                        {touched.currentPassword && errors.currentPassword && <div className="text-danger mt-3">{errors?.currentPassword}</div>}
+                      </div>
+                    </div>
+                    <div className="row m-0 pt-3">
+                      <div className="col-md-4 pt-1 px-0">
+                        <label htmlFor="newPassword" className="font-weight-bold">New password:</label>
+                      </div>
+                      <div className="col-md-8 px-0">
+                        <Field
+                          type="password"
+                          className={className('form-control', { 'border-danger': errors.newPassword })}
+                          name="newPassword"
+                          id="newPassword"
+                          placeholder="New password"
+                        />
+                        {touched.newPassword && errors.newPassword && <div className="text-danger mt-3">{errors?.newPassword}</div>}
+                      </div>
+                    </div>
+                    <div className="row m-0 pt-3">
+                      <div className="col-md-4 pt-1 px-0">
+                        <label htmlFor="confirmNewPassword" className="font-weight-bold">Confirm password:</label>
+                      </div>
+                      <div className="col-md-8 px-0">
+                        <Field
+                          type="password"
+                          className={className('form-control', { 'border-danger': errors.confirmNewPassword })}
+                          name="confirmNewPassword"
+                          id="confirmNewPassword"
+                          placeholder="Confirm password"
+                        />
+                        {touched.confirmNewPassword && errors.confirmNewPassword && <div className="text-danger mt-3">{errors?.confirmNewPassword}</div>}
+                      </div>
+                    </div>
+                    <div className="row m-0 py-3">
+                      <div className="col-md-3 col-sm-4 col-6 pl-0 pr-1">
+                        <Button
+                          type="button"
+                          className="w-100"
+                          variant="secondary"
+                          onClick={() => { history.push(paths.MY_PROFILE); }}
+                        >
+                          Cancel
+                        </Button>
+                      </div>
+                      <div className="col-md-3 offset-md-6 col-sm-4 offset-sm-4 col-6 pl-1 pr-0">
+                        <Button
+                          type="submit"
+                          className=" w-100"
+                          variant="success"
+                          disabled={!isValid || !dirty || isChangePasswordLoading
+                          || errors.currentPassword || errors.newPassword
+                          || errors.confirmNewPassword}
+                        >
+                          Save
+                        </Button>
+                      </div>
                     </div>
                   </div>
-                  <div className="row m-0 pt-3">
-                    <div className="col-md-4 pt-1 px-0">
-                      <label htmlFor="currentPassword" className="font-weight-bold">Current password:</label>
-                    </div>
-                    <div className="col-md-8 px-0">
-                      <Field
-                        type="password"
-                        className={className('form-control', { 'border-danger': errors.currentPassword })}
-                        name="currentPassword"
-                        id="currentPassword"
-                        placeholder="Current password"
-                      />
-                      {touched.currentPassword && errors.currentPassword && <div className="text-danger mt-3">{errors?.currentPassword}</div>}
-                    </div>
-                  </div>
-                  <div className="row m-0 pt-3">
-                    <div className="col-md-4 pt-1 px-0">
-                      <label htmlFor="newPassword" className="font-weight-bold">New password:</label>
-                    </div>
-                    <div className="col-md-8 px-0">
-                      <Field
-                        type="password"
-                        className={className('form-control', { 'border-danger': errors.newPassword })}
-                        name="newPassword"
-                        id="newPassword"
-                        placeholder="New password"
-                      />
-                      {touched.newPassword && errors.newPassword && <div className="text-danger mt-3">{errors?.newPassword}</div>}
-                    </div>
-                  </div>
-                  <div className="row m-0 pt-3">
-                    <div className="col-md-4 pt-1 px-0">
-                      <label htmlFor="confirmNewPassword" className="font-weight-bold">Confirm password:</label>
-                    </div>
-                    <div className="col-md-8 px-0">
-                      <Field
-                        type="password"
-                        className={className('form-control', { 'border-danger': errors.confirmNewPassword })}
-                        name="confirmNewPassword"
-                        id="confirmNewPassword"
-                        placeholder="Confirm password"
-                      />
-                      {touched.confirmNewPassword && errors.confirmNewPassword && <div className="text-danger mt-3">{errors?.confirmNewPassword}</div>}
-                    </div>
-                  </div>
-                  <div className="row m-0 py-3">
-                    <div className="col-md-3 col-sm-4 col-6 pl-0 pr-1">
-                      <Button
-                        className="w-100"
-                        variant="secondary"
-                        onClick={() => { history.push(paths.MY_PROFILE); }}
-                      >
-                        Cancel
-                      </Button>
-                    </div>
-                    <div className="col-md-3 offset-md-6 col-sm-4 offset-sm-4 col-6 pl-1 pr-0">
-                      <Button
-                        variant="success"
-                        className=" w-100"
-                        type="submit"
-                        disabled={!isValid || !dirty}
-                      >
-                        Save
-                      </Button>
-                    </div>
-                  </div>
-                </div>
-              </Form>
-            )}
-          </Formik>
+                </Form>
+              )}
+            </Formik>
+            <ModalWindow
+              toShow={toShowModal}
+              onSubmit={handleConfirm}
+              onClose={handleCloseModal}
+            >Are you sure you want to change password?
+            </ModalWindow>
+          </WithLoading>
         </div>
       </div>
     </div>
