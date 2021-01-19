e2028443dacf8787e9a8dbefc7850fe707cdbac7
CoderGTR <pavlov.vlad.nlu@gmail.com>
2021-01-19 20:34:57 +0200

    resolve conflicts

--- src/shared/api-service/config.js
+++ src/shared/api-service/config.js
@@ -4,8 +4,5 @@ export const BASE_URL = 'https://whatbackendapi.azurewebsites.net/api';
 
 // export const BASE_URL = 'https://whatproject.azurewebsites.net/api';
 
-// <<<<<<< HEAD
 // export const BASE_URL = 'https://what-project.azurewebsites.net/api';
-// =======
-// export const BASE_URL = 'https://what-project.azurewebsites.net/api';
-// >>>>>>> 4e8b0b695d2976ff8f9450169c61a84554f18f39
+
