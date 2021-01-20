2bbca7bcbc95fc6ba4d62084c33dc05e13250c17
Mozdolevskyi Maksym <mexickoe@gmail.com>
2021-01-20 19:05:13 +0200

    list of mentors is finished

--- src/features/mentors/list-of-mentors/list-of-mentors.js
+++ src/features/mentors/list-of-mentors/list-of-mentors.js
@@ -2,10 +2,9 @@ import React, { useCallback, useEffect, useState } from 'react';
 import { shallowEqual, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { paths, useActions } from '@/shared';
-import { currentUserSelector, fetchMentors, fetchActiveMentors, mentorsSelector, mentorsActiveSelector, loadActiveStudents } from '@/models';
+import { currentUserSelector, fetchMentors, fetchActiveMentors, mentorsSelector, mentorsActiveSelector } from '@/models';
 
 import { Button, Pagination, Search, WithLoading } from '@/components';
-import Icon from '@/icon.js';
 
 import classNames from 'classnames';
 import styles from './list-of-mentors.scss';
@@ -17,24 +16,17 @@ const editIcon = (
 </svg>
 );
 
-const icon1 = (
+const iconTable = (
   <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-layout-text-sidebar" viewBox="0 0 16 16">
     <path d="M3.5 3a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zm0 3a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zM3 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5z"/>
     <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm12-1v14h2a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1h-2zm-1 0H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h9V1z"/>
   </svg>
 );
 
-const icon2 = (
+const iconCards = (
   <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-grid" viewBox="0 0 16 16">
     <path d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5v-3zM2.5 2a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zm6.5.5A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zM1 10.5A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zm6.5.5A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3z"/>
   </svg>
-);
-
-const arrow = (
-  <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-arrow-down" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-    <path fillRule="evenodd" d="M4.646 9.646a.5.5 0 0 1 .708 0L8 12.293l2.646-2.647a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 0 1 0-.708z"/>
-    <path fillRule="evenodd" d="M8 2.5a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-1 0V3a.5.5 0 0 1 .5-.5z"/>
-</svg>
 );
 
 export const ListOfMentors = () => {
@@ -66,14 +58,22 @@ export const ListOfMentors = () => {
   const indexOfLastMentor = currentPage * mentorsPerPage;
   const indexOfFirstMentor = indexOfLastMentor - mentorsPerPage;
 
+  const getDisabledMentors = () => {
+    const activeMentorIds = activeMentors.map(({ id }) => id);
+
+    return allMentors.filter(({ id }) => !activeMentorIds.includes(id));
+  };
+
+  const searchMentors = (searchedMentors) => searchedMentors.filter(({ firstName, lastName }) => `${firstName} ${lastName}`
+    .toLowerCase().includes(searchMentorValue.toLowerCase()));
+
   useEffect(() => {
     loadActiveMentors()
   }, [loadActiveMentors]);
   
   useEffect(() => {
     if (isShowDisabled) {
-      const activeMentorIds = activeMentors.map(({ id }) => id);
-      const disabledMentors = allMentors.filter(({ id }) => !activeMentorIds.includes(id));
+      const disabledMentors = getDisabledMentors();
 
       setFilteredMentorList(disabledMentors.map((mentor, index) => ({ index, ...mentor })));
     }
@@ -86,36 +86,34 @@ export const ListOfMentors = () => {
   [activeMentors, areActiveMentorsLoading, allMentors, areAllMentorsLoading, isShowDisabled]);
 
   useEffect(() => {
-    if (isShowDisabled) {
-      const activeMentorIds = activeMentors.map(({ id }) => id);
-      const disabledMentors = allMentors.filter(({ id }) => !activeMentorIds.includes(id));
+    setVisibleMentors(filteredMentorList.slice(indexOfFirstMentor, indexOfLastMentor));
+  }, [currentPage, filteredMentorList]);
 
-      const searchedMentors = disabledMentors.filter(({ firstName, lastName }) => `${firstName} ${lastName}`
-        .toLowerCase().includes(searchMentorValue.toLowerCase()));
+  useEffect(() => {
+    if (isShowDisabled) {
+      const disabledMentors = getDisabledMentors();
+      const searchedMentors = searchMentors(disabledMentors);
 
       setFilteredMentorList(searchedMentors.map((mentor, index) => ({ index, ...mentor })));
     } else {
-      const searchedMentors = activeMentors.filter(({ firstName, lastName }) => `${firstName} ${lastName}`
-        .toLowerCase().includes(searchMentorValue.toLowerCase()));
-
-        setFilteredMentorList(searchedMentors.map((mentor, index) => ({ index, ...mentor })));
+      const searchedMentors = searchMentors(activeMentors);
+      
+      setFilteredMentorList(searchedMentors.map((mentor, index) => ({ index, ...mentor })));
     }
   }, [searchMentorValue, isShowDisabled]);
-
-  useEffect(() => {
-    setVisibleMentors(filteredMentorList.slice(indexOfFirstMentor, indexOfLastMentor));
-  }, [currentPage, filteredMentorList]);
 
   const mentorList = () => {
     const mentors = visibleMentors
       .map(({ id, index, firstName, lastName, email}) => {
         return (
-          <tr onClick={() => mentorDetails(id)} key={id} className={styles.pointer} data-mentor-id={id}>
+          <tr onClick={() => mentorDetails(id)} key={id} className={styles["table-rows"]} data-mentor-id={id}>
             <td className="text-center">{index + 1}</td>
             <td>{firstName}</td>
             <td>{lastName}</td>
             <td>{email}</td>
-            {currentUser.role !== 2 ? <td className="text-center" data-mentor-id={id} onClick={(event) => mentorEdit(event, id) }>{editIcon}</td> : null}
+            {currentUser.role !== 2 ? 
+              <td className="text-center" data-mentor-id={id} onClick={(event) => mentorEdit(event, id) }>{editIcon}</td> : null
+            }
           </tr>
         );
       })
@@ -161,9 +159,9 @@ export const ListOfMentors = () => {
     setSearchMentorValue(inputValue);
   };
 
-  const addMentor = () => {
+  const addMentor = useCallback(() => {
     history.push(paths.UNASSIGNED_USERS);
-  };
+  }); 
 
   const mentorDetails = useCallback((id) => {
     history.push(`${paths.MENTORS_DETAILS}/${id}`);
@@ -182,32 +180,21 @@ export const ListOfMentors = () => {
 
   const nextPage = (pageNumber) => {
     const totalPages = Math.ceil(activeMentors?.length / mentorsPerPage);
-    setCurrentPage((prev) => {
-      if (prev === totalPages) {
-        return prev;
-      } else {
-        return pageNumber;
-      }
-    });
+
+    setCurrentPage(currentPage === totalPages ? currentPage : pageNumber);
   };
 
   const prevPage =(pageNumber) => {
-    setCurrentPage((prev) => {
-      if (prev - 1 === 0) {
-        return prev;
-      } else {
-        return pageNumber;
-      }
-    });
+    setCurrentPage(currentPage - 1 === 0 ? currentPage : pageNumber);
   };
-
-  console.log(filteredMentorList.length)
 
   return (
     <div className="container">
-      <div className="row d-flex justify-content-between align-items-center mb-3">
+      <div className="row justify-content-between align-items-center mb-3">
         <div className="col-6"><h2>Mentors</h2></div>
-        {filteredMentorList.length > mentorsPerPage ? <span className="col-2 text-right">{filteredMentorList.length} mentors</span> : null}
+        {filteredMentorList.length > mentorsPerPage ? 
+          <span className="col-2 text-right">{filteredMentorList.length} mentors</span> : null
+        }
         <div className="col-4 d-flex align-items-center justify-content-end">
           {filteredMentorList.length > mentorsPerPage && !areActiveMentorsLoading && !areAllMentorsLoading &&
             <Pagination
@@ -218,8 +205,7 @@ export const ListOfMentors = () => {
               nextPage={nextPage}
               page={currentPage}
             />
-          }
-          
+          }  
         </div>
       </div>
       <div className="row justify-content-center">
@@ -227,54 +213,65 @@ export const ListOfMentors = () => {
           <div className="px-3 py-2 mb-2">
             <div className="row align-items-center">
               <div className="col-2">
-                <button className="btn">{icon1}</button>
-                <button className="btn">{icon2}</button>
+                <button className="btn">{iconTable}</button>
+                <button className="btn">{iconCards}</button>
               </div>
               <div className="col-4">
                 <Search onSearch={handleSearch} placeholder="Mentor's name" />
               </div>
               {currentUser.role !== 2 && 
               <div className="custom-control custom-switch col-2 offset-2">
-                <input onClick={handleShowDisabled} type="checkbox" className="custom-control-input" id="customSwitch1"/>
-                <label className="custom-control-label" htmlFor="customSwitch1">Disabled Mentors</label>
+                <input 
+                  onClick={handleShowDisabled} 
+                  type="checkbox" 
+                  className={classNames("custom-control-input", styles["switch-input"])} 
+                  id="disabledMentors"
+                />
+                <label 
+                  className={classNames("custom-control-label", styles["switch-label"])} 
+                  htmlFor="disabledMentors"
+                >
+                  Disabled Mentors
+                </label>
               </div>}
               <div className="col-2">
                 {currentUser.role !== 2 &&
                   <Button onClick={addMentor} className={styles.btn}>
-                    <Icon icon="Plus" className="icon" />
                     <span>Add a mentor</span>
                   </Button>
                 }
               </div>
             </div>
           </div>
-          <div className="row">
-            <WithLoading isLoading={areActiveMentorsLoading || areAllMentorsLoading} className="d-block mx-auto m-0">
-              <table className="table table-hover">
-                <thead>
-                  <tr>
-                    {sortingCategories.map(({ id, name, tableHead, sortedByAscending }) => (
-                      <th
-                        className={classNames(styles.th, {[styles.throtate]: sortedByAscending})}
-                        key={id}
-                        onClick={handleSortByParam}
-                        data-sorting-param={name}
-                        data-sorted-by-ascending={Number(sortedByAscending)}
+          <WithLoading isLoading={areActiveMentorsLoading || areAllMentorsLoading} className="d-block mx-auto m-0">
+            <table className="table table-hover">
+              <thead>
+                <tr>
+                  {sortingCategories.map(({ id, name, tableHead, sortedByAscending }) => (
+                    <th   
+                      key={id}
+                      className={styles["table-head"]}
+                    >
+                      <span 
+                        data-sorting-param={name} 
+                        data-sorted-by-ascending={Number(sortedByAscending)} 
+                        onClick={handleSortByParam} 
+                        className={classNames({[styles.rotate]: sortedByAscending})}
                       >
                         {tableHead}
-                      </th>
-                    ))}
-                    {currentUser.role !== 2 ? <th scope="col" className="text-center">Edit</th> : null}
-                  </tr>
-                </thead>
-                <tbody>
-                  {
-                    mentorList()
-                  }
-                </tbody>
-              </table>
-            </WithLoading>
-          </div>
+                      </span>
+                    </th>
+                  ))}
+                  {currentUser.role !== 2 ? <th scope="col" className="text-center">Edit</th> : null}
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  mentorList()
+                }
+              </tbody>
+            </table>
+          </WithLoading>
         </div>
       </div>
     </div>
