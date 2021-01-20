164f2408af26754485927dd8fabaae8629a9e7a2
Marina Pasternak <256pasternakmarina@gmail.com>
2021-01-20 03:52:55 +0300

    add group starting

--- src/models/student-groups/actions.js
+++ src/models/student-groups/actions.js
@@ -69,7 +69,7 @@ function* watchEditingStudentGroups() {
 }
 
 export const addStudentGroup = (group) => ({
-  type: actionTypes.EDIT_STUDENT_GROUP,
+  type: actionTypes.ADD_STUDENT_GROUP,
   payload: group,
 });
 
@@ -77,7 +77,7 @@ function* addStudentGroupAsync({ payload }) {
   try {
     yield put({ type: actionTypes.ADDING_STUDENT_GROUP_STARTED });
 
-    const group = yield call(ApiService.create, `/student_groups/${payload.id}`, payload);
+    const group = yield call(ApiService.create, '/student_groups/', payload);
 
     yield put({ type: actionTypes.ADDING_STUDENT_GROUP_SUCCEED, payload: group });
   } catch (error) {
