2a74621a5772268ed417fa9a0dab7bfdaf2d1de8
CoderGTR <pavlov.vlad.nlu@gmail.com>
2021-01-20 14:36:12 +0200

    chenged layout of groups list. Without sort.

--- src/features/groups/list-of-groups/list-of-groups.js
+++ src/features/groups/list-of-groups/list-of-groups.js
@@ -9,6 +9,28 @@ import { globalLoadStudentGroups, loadStudentGroupsSelector } from '@models/inde
 import classNames from 'classnames';
 import styles from './list-of-groups.scss';
 import Icon from '@/icon.js';
+import {icons} from "react-icons";
+
+const editIcon = (
+    <svg width="1.1em" height="1.1em" viewBox="0 0 16 16" className={classNames("bi bi-pencil", styles.scale)} fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+      <path fillRule="evenodd" d="M11.293 1.293a1 1 0 0 1 1.414 0l2 2a1 1 0 0 1 0 1.414l-9 9a1 1 0 0 1-.39.242l-3 1a1 1 0 0 1-1.266-1.265l1-3a1 1 0 0 1 .242-.391l9-9zM12 2l2 2-9 9-3 1 1-3 9-9z"/>
+      <path fillRule="evenodd" d="M12.146 6.354l-2.5-2.5.708-.708 2.5 2.5-.707.708zM3 10v.5a.5.5 0 0 0 .5.5H4v.5a.5.5 0 0 0 .5.5H5v.5a.5.5 0 0 0 .5.5H6v-1.5a.5.5 0 0 0-.5-.5H5v-.5a.5.5 0 0 0-.5-.5H3z"/>
+    </svg>
+);
+
+const icon1 = (
+    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-layout-text-sidebar" viewBox="0 0 16 16">
+      <path d="M3.5 3a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zm0 3a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zM3 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5z"/>
+      <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm12-1v14h2a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1h-2zm-1 0H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h9V1z"/>
+    </svg>
+);
+
+const icon2 = (
+    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-grid" viewBox="0 0 16 16">
+      <path d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5v-3zM2.5 2a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zm6.5.5A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zM1 10.5A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zm6.5.5A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3z"/>
+    </svg>
+);
+
 
 export const ListOfGroups = () => {
   const history = useHistory();
@@ -18,6 +40,8 @@ export const ListOfGroups = () => {
 
   const [currentPage, setCurrentPage] = useState(1);
   const [groupsPerPage] = useState(12);
+
+  const [visibleGroups, setVisibleGroups] = useState([]);
 
   const { setSearchGroupValue, inputGroupStartDate } = useActions(listOfGroupsActions);
   const searchGroupName = useSelector(searchGroup, shallowEqual);
@@ -65,18 +89,30 @@ export const ListOfGroups = () => {
       .sort((a, b) => {
         return a.startDate < b.startDate ? -1 : a.startDate > b.startDate ? 1 : 0;
       })
-      .map((group) => (
-        <Card
-          key={group.id}
-          id={group.id}
-          title={group.name}
-          date={group.startDate.replaceAll('-', '.').slice(0, 10).split('.').reverse().join('.')}
-          buttonName="Details"
-          iconName="Edit"
-          onEdit={() => handleCardEdit(group.id)}
-          onDetails={() => handleCardDetails(group.id)}
-        />
-      ));
+        .map((group, index) => {
+          return (
+            <tr  className={styles['table-item']} onClick={() => handleCardDetails(group.id)} key={group.id}>
+              <td>{index+1}</td>
+              <td>{group.name}</td>
+              <td>{group.studentIds.length}</td>
+              <td>{group.startDate.replaceAll('-', '.').slice(0, 10).split('.').reverse().join('.')}</td>
+              <td onClick={() => handleCardEdit(group.id)}>{editIcon}</td>
+            </tr>
+        )});
+      // .map((group) => (
+      //   // <Card
+      //   //   key={group.id}
+      //   //   id={group.id}
+      //   //   title={group.name}
+      //   //   date={group.startDate.replaceAll('-', '.').slice(0, 10).split('.').reverse().join('.')}
+      //   //   buttonName="Details"
+      //   //   iconName="Edit"
+      //   //   onEdit={() => handleCardEdit(group.id)}
+      //   //   onDetails={() => handleCardDetails(group.id)}
+      //   // />
+      //
+      //
+      // ));
 
       if (!groupList.length && searchGroupName || searchStartDate) {
         return <h4>Group is not found</h4>;
@@ -116,8 +152,35 @@ export const ListOfGroups = () => {
 
   return (
     <div className={classNames("container", styles['list-wrapper'])}>
-      <div className="row">
-        <div className="col-md-2 text-left">
+      <div className="row d-flex justify-content-between align-items-center mb-3">
+        <div className="col-6">
+          <h1>Groups</h1>
+        </div>
+        <div className="col-4 d-flex align-items-center justify-content-end">
+
+            {listByDate.length > 12 && !isLoading &&
+            <Pagination
+                itemsPerPage={groupsPerPage}
+                totalItems={listByDate.length}
+                paginate={paginate}
+                prevPage={prevPage}
+                nextPage={nextPage}
+            />
+            }
+
+        </div>
+      </div>
+      <div className={classNames(styles['body-wrapper'], 'shadow p-3')}>
+      <div className="row d-flex justify-content-between">
+
+        <div className="col-2">
+          <button className="btn">{icon1}</button>
+          <button className="btn">{icon2}</button>
+        </div>
+        <div className="col-3  pl-3">
+          <Search onSearch={handleSearch} placeholder="Group's name" />
+        </div>
+        <div className="col-2 text-center">
           <input
             className={classNames('form-control ', styles['calendar-input'])}
             type="date"
@@ -127,11 +190,13 @@ export const ListOfGroups = () => {
             placeholder="year-month-day"
           />
         </div>
-        <div className="col-md-4 offset-md-2 text-center pl-3">
-          <Search onSearch={handleSearch} placeholder="Group's name" />
-        </div>
-          <div className="col-md-4 col-12 text-right">
-            <Button onClick={handleAddGroup} variant="warning">
+
+        {/*<div className="custom-control custom-switch col-md-2  d-flex align-items-center">*/}
+        {/*  <input type="checkbox" className="custom-control-input" id="customSwitch1"/>*/}
+        {/*  <label className="custom-control-label" htmlFor="customSwitch1">Disabled Students</label>*/}
+        {/*</div>*/}
+          <div className="col-2 d-flex justify-content-end">
+            <Button onClick={handleAddGroup} className={styles['btn-add']}>
               <Icon icon="Plus" className="icon" />
                 Add a group
             </Button>
@@ -141,19 +206,36 @@ export const ListOfGroups = () => {
           <hr className="col-8" />
           <div className="col-12 d-flex flex-row flex-wrap justify-content-center">
             <WithLoading isLoading={isLoading}>
+            <table className="table">
+              <thead>
+              <tr>
+                <th scope="col">№</th>
+                <th scope="col">Group name</th>
+                <th scope="col">Quantity of students</th>
+                <th scope="col">Date of start</th>
+                <th scope="col">Edit</th>
+              </tr>
+              </thead>
+
+
+              <tbody>
               {getGroupList()}
+              </tbody>
+
+            </table>
             </WithLoading>
           </div>
         </div>
-      {listByDate.length > 12 && !isLoading &&
-        <Pagination 
-          itemsPerPage={groupsPerPage} 
-          totalItems={listByDate.length} 
-          paginate={paginate}
-          prevPage={prevPage}
-          nextPage={nextPage}
-        />
-      }
+      {/*{listByDate.length > 12 && !isLoading &&*/}
+      {/*  <Pagination*/}
+      {/*    itemsPerPage={groupsPerPage}*/}
+      {/*    totalItems={listByDate.length}*/}
+      {/*    paginate={paginate}*/}
+      {/*    prevPage={prevPage}*/}
+      {/*    nextPage={nextPage}*/}
+      {/*  />*/}
+      {/*}*/}
+      </div>
     </div>
   );
 };
