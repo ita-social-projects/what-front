cf81c45d21e169467d9d6064bb9ccf2b4f344a4a
Drozdov Oleg <drozdov.oleg87@gmail.com>
2021-02-05 17:01:05 +0200

    added route to resetPassword

--- src/shared/routes/routes.js
+++ src/shared/routes/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import { ProtectedRoute } from '@/components/index.js';
-import { NotFound, Auth, Registration, Layout, Counter, ForgotPassword } from '../../features/index.js';
+import { NotFound, Auth, Registration, Layout, Counter, ForgotPassword, ResetPassword } from '../../features/index.js';
 import { paths } from './index.js';
 
 export const Routes = () => (
@@ -12,6 +12,7 @@ export const Routes = () => (
       <Route exact path={paths.AUTH} component={Auth} />
       <Route exact path={paths.REGISTRATION} component={Registration} />
       <Route exact path={paths.FORGOT_PASSWORD} component={ForgotPassword} />
+      <Route exact path={paths.RESET_PASSWORD} component={ResetPassword} />
       <Route exact path={paths.COUNTER} component={Counter} />
       <ProtectedRoute roles={[1, 2, 3, 4]} path={paths.HOME} component={Layout} />
     </Switch>
