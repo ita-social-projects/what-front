4f3f7ce1855ebe9058f4e90dd591f51a994ba4dc
Drozdov Oleg <drozdov.oleg87@gmail.com>
2021-02-12 15:43:08 +0200

    hot fix

--- src/shared/api-service/config.js
+++ src/shared/api-service/config.js
@@ -1,4 +1,4 @@
-// export const BASE_URL = 'https://whatbackendapi.azurewebsites.net/api';
+export const BASE_URL = 'https://whatbackendapi.azurewebsites.net/api';
 
 // export const BASE_URL = 'https://dp192-what-api.oa.r.appspot.com/api';
 
@@ -6,4 +6,4 @@
 
 // export const BASE_URL = 'https://what-project.azurewebsites.net/api';
 
-export const BASE_URL = 'https://whatbackendapihosting.azurewebsites.net/api';
+// export const BASE_URL = 'https://whatbackendapihosting.azurewebsites.net/api';
