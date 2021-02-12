86047cc0793676fd287e1227d232e3033eb1a33f
Drozdov Oleg <drozdov.oleg87@gmail.com>
2021-02-12 15:26:01 +0200

    added new base url

--- src/shared/api-service/config.js
+++ src/shared/api-service/config.js
@@ -1,7 +1,9 @@
-export const BASE_URL = 'https://whatbackendapi.azurewebsites.net/api';
+// export const BASE_URL = 'https://whatbackendapi.azurewebsites.net/api';
 
 // export const BASE_URL = 'https://dp192-what-api.oa.r.appspot.com/api';
 
 // export const BASE_URL = 'https://whatproject.azurewebsites.net/api';
 
 // export const BASE_URL = 'https://what-project.azurewebsites.net/api';
+
+export const BASE_URL = 'https://whatbackendapihosting.azurewebsites.net/api';
