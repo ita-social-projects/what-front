9525c0897d78ff4d5de2c5af06925eab43b5b0df
DzigMS <dzigmihail@gmail.com>
2021-04-08 14:19:34 +0300

    Change link to backEnd

--- src/shared/api-service/config.js
+++ src/shared/api-service/config.js
@@ -1,4 +1,6 @@
-export const BASE_URL = 'https://whatbackendapi.azurewebsites.net/api';
+export const BASE_URL = 'https://charliebackendapi20210401200736.azurewebsites.net/api';
+
+//export const BASE_URL = 'https://whatbackendapi.azurewebsites.net/api';
 
 // export const BASE_URL = 'https://dp192-what-api.oa.r.appspot.com/api';
 
