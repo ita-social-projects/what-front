318d8f1fa3b9398f20a0fe3e0e341679ea819a9c
DzigMS <dzigmihail@gmail.com>
2021-05-14 12:44:50 +0300

    Update config.js

--- src/shared/api-service/config.js
+++ src/shared/api-service/config.js
@@ -1,9 +1,1 @@
-export const BASE_URL = 'https://charliebackendapi20210401200736.azurewebsites.net/api';
-
-//export const BASE_URL = 'https://whatbackendapi.azurewebsites.net/api';
-
-// export const BASE_URL = 'https://dp192-what-api.oa.r.appspot.com/api';
-
-// export const BASE_URL = 'https://whatproject.azurewebsites.net/api';
-
-// export const BASE_URL = 'https://what-project.azurewebsites.net/api';
+export const BASE_URL = 'https://whatbackend.azurewebsites.net/api';
