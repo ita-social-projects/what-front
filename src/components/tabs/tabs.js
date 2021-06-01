c30d8de75677c705e2f05cb42bf60226c22b988b
7SergSmith7 <serg.kuznetsov89@gmail.com>
2021-06-01 19:50:30 +0300

    fix-format:Misprint-and-reformation-problem

--- src/components/tabs/tabs.js
+++ src/components/tabs/tabs.js
@@ -3,14 +3,22 @@ import { Link } from 'react-router-dom';
 import propTypes from 'prop-types';
 
 const arrow = (
-  <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-arrow-left" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-    <path fillRule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
+  <svg
+    width="1em"
+    height="1em"
+    viewBox="0 0 16 16"
+    className="bi bi-arrow-left"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fillRule="evenodd"
+      d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
+    />
   </svg>
 );
 
-export const Tabs = ({
-  className, children, defaultIndex, linkBack,
-}) => {
+export const Tabs = ({ className, children, defaultIndex, linkBack }) => {
   const [tabs, setTabs] = useState({
     activeTabIndex: defaultIndex,
   });
@@ -30,25 +38,29 @@ export const Tabs = ({
     }
   };
 
-  const renderTabs = () => React.Children.map(children, (child, index) => React.cloneElement(child, {
-    tabIndex: index,
-    isActive: index === activeTabIndex,
-    onClick: toggleActiveTab,
-  }));
+  const renderTabs = () =>
+    React.Children.map(children, (child, index) =>
+      React.cloneElement(child, {
+        tabIndex: index,
+        isActive: index === activeTabIndex,
+        onClick: toggleActiveTab,
+      })
+    );
 
   return (
     <div className={className}>
       <div className="nav nav-tabs">
-        {/* <Link
-          className="nav-item nav-link d-flex align-items-center"
-          to={linkBack}
-        >{arrow}
-        </Link> */}
+        {
+          <Link
+            className="nav-item nav-link d-flex align-items-center"
+            to={linkBack}
+          >
+            {arrow}
+          </Link>
+        }
         {renderTabs()}
       </div>
-      <div>
-        {renderContent()}
-      </div>
+      <div>{renderContent()}</div>
     </div>
   );
 };
