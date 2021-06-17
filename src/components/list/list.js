2da5f851894c156b52c440327f75d82a5e000e88
Kostyantyn Kryoka <42357921+ferbiy@users.noreply.github.com>
2021-06-18 00:51:12 +0300

    Feat: added new logic for edit restriction; added more documantation

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -2,21 +2,23 @@ import React from 'react';
 import styles from './list.scss';
 import Icon from '../../icon.js';
 
-const getRows = (data, handleDetails, handleEdit) =>
+const getRows = (data, handleDetails, handleEdit, editRestriction = false) =>
     data.map(({id, index, firstName, lastName, email}) =>
         <tr key={id}
             onClick={() => handleDetails(id)}
-            data-student-id={id}
             className={styles['table-row']}
         >
             <td className="text-center">{index + 1}</td>
             <td>{firstName}</td>
             <td>{lastName}</td>
             <td>{email}</td>
-            <td className="text-center"
-                onClick={(event) => handleEdit(event, id)}>
-                <Icon icon="Edit" className={styles.scale} color="#2E3440" size={30}/>
-            </td>
+            { !editRestriction ?
+                (<td className="text-center"
+                    onClick={(event) => handleEdit(event, id)}>
+                    <Icon icon="Edit" className={styles.scale} color="#2E3440" size={30}/>
+                </td>) :
+                <td></td>
+            }
         </tr>
     );
 
@@ -60,16 +62,20 @@ const getBlocks = (data, handleDetails, handleEdit) =>
     data: Array<Object{id, index, firstName, lastName, email}>,
     handleDetails: function,
     handleEdit: function,
-    errors: Array<Object{message, check: [boolean, ...]}>
+    errors: Array<Object{message, check: [boolean, ...]}>,
+    editRestriction: boolean
   }
  * @returns markup in rows (listType === 'list') or in blocks/cards (listType === 'block')
+ *
+ * errors contains objects with check array (boolean values) and message field when at least one error is true
+ * editRestriction contains boolean if user has role with limitation for editing, it is false by default
  */
 
 export const List = ({listType, props}) => {
-    const {data, handleDetails, handleEdit, errors} = props;
+    const {data, handleDetails, handleEdit, errors, editRestriction} = props;
 
     const markup = (listType === 'list')
-        ? getRows(data, handleDetails, handleEdit)
+        ? getRows(data, handleDetails, handleEdit, editRestriction)
         : getBlocks(data, handleDetails, handleEdit);
 
     const returnError = {isError: false, message: ''};
