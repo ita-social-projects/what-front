11fddc61a52fec6ca9af4fdd6e9c976d9dd73101
Kostyantyn Kryoka <42357921+ferbiy@users.noreply.github.com>
2021-06-17 23:26:15 +0300

    Feat: documentation for List component, fixed code-style, removed todo comment

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -2,77 +2,90 @@ import React from 'react';
 import styles from './list.scss';
 import Icon from '../../icon.js';
 
-//todo fix style's classes, add cursor point for edit btn
-
 const getRows = (data, handleDetails, handleEdit) =>
-    data.map(({id, index, firstName, lastName, email} ) =>
+    data.map(({id, index, firstName, lastName, email}) =>
         <tr key={id}
             onClick={() => handleDetails(id)}
             data-student-id={id}
             className={styles['table-row']}
         >
-          <td className="text-center">{index + 1}</td>
-          <td>{firstName}</td>
-          <td>{lastName}</td>
-          <td>{email}</td>
-          <td className="text-center"
-              onClick={(event) => handleEdit(event, id)}>
-            <Icon icon="Edit" className={styles.scale} color="#2E3440" size={30}/>
-          </td>
+            <td className="text-center">{index + 1}</td>
+            <td>{firstName}</td>
+            <td>{lastName}</td>
+            <td>{email}</td>
+            <td className="text-center"
+                onClick={(event) => handleEdit(event, id)}>
+                <Icon icon="Edit" className={styles.scale} color="#2E3440" size={30}/>
+            </td>
         </tr>
-);
+    );
 
 const getBlocks = (data, handleDetails, handleEdit) =>
     data.map(({id, index, firstName, lastName, email}) =>
         <div className="card"
              style={{
-               width: '31%',
-               margin: '1%',
-               cursor: 'pointer'
+                 width: '31%',
+                 margin: '1%',
+                 cursor: 'pointer'
              }}
              onClick={() => handleDetails(id)}
              key={id}>
-          <div className="card-body d-flex justify-content-between">
-            <div>{index + 1}</div>
-            <div>
-              <div>
-                {firstName}
-              </div>
-              <div>
-                {lastName}
-              </div>
-              <div>
-                {email}
-              </div>
+            <div className="card-body d-flex justify-content-between">
+                <div>{index + 1}</div>
+                <div>
+                    <div>
+                        {firstName}
+                    </div>
+                    <div>
+                        {lastName}
+                    </div>
+                    <div>
+                        {email}
+                    </div>
+                </div>
+                <Icon icon="Edit"
+                      onClick={(event) => handleEdit(event, id)}
+                      className={styles.scale}
+                      color="#2E3440"
+                      size={30}/>
             </div>
-            <Icon icon="Edit"
-                  onClick={(event) => handleEdit(event, id)}
-                  className={styles.scale}
-                  color="#2E3440"
-                  size={30}/>
-          </div>
         </div>
-);
+    );
+
+
+/**
+ * @param listType may be 'list' or 'block'
+ * @param props is an object and has structure:
+ * {
+    data: Array<Object{id, index, firstName, lastName, email}>,
+    handleDetails: function,
+    handleEdit: function,
+    errors: Array<Object{message, check: [boolean, ...]}>
+  }
+ * @returns markup in rows (listType === 'list') or in blocks/cards (listType === 'block')
+ */
 
 export const List = ({listType, props}) => {
-const { data, handleDetails, handleEdit, errors } = props;
+    const {data, handleDetails, handleEdit, errors} = props;
 
-  const markup = (listType === 'list')
-      ? getRows(data, handleDetails, handleEdit)
-      : getBlocks(data, handleDetails, handleEdit);
+    const markup = (listType === 'list')
+        ? getRows(data, handleDetails, handleEdit)
+        : getBlocks(data, handleDetails, handleEdit);
 
-  const returnError = {isError: false, message: ''};
+    const returnError = {isError: false, message: ''};
 
-  errors.forEach(error => {
-    if(error.check.includes(true)){
-      returnError.isError = true;
-      returnError.message = error.message;
+    errors.forEach(error => {
+        if (error.check.includes(true)) {
+            returnError.isError = true;
+            returnError.message = error.message;
+        }
+    });
+
+    if (returnError.isError) {
+        return <tr>
+            <td colSpan="5" className="text-center">{returnError.message}</td>
+        </tr>;
     }
-  });
 
-  if (returnError.isError) {
-    return <tr><td colSpan="5" className="text-center">{returnError.message}</td></tr>;
-  }
-
-  return markup;
+    return markup;
 };
