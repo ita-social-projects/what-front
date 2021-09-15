1b579f5f8d9a9d5feda858bcd0013ae09db5de4b
Volodymyr Zenin <vovan1608@gmail.com>
2021-09-15 19:43:26 +0300

    added test for loaders edit-students-component

--- src/features/students/edit-students-details/__tests__/edit-students-details.test.js
+++ src/features/students/edit-students-details/__tests__/edit-students-details.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
 import { Router } from 'react-router-dom';
-import { paths } from '@/shared';
+import { paths, useActions } from '@/shared';
 import { useSelector } from 'react-redux';
 
 import {
@@ -12,40 +12,117 @@ import {
 
 import { EditStudentsDetails } from '../edit-students-details.js';
 
-jest.mock('react-redux', () => ({useSelector: jest.fn()}));
-jest.mock('@/shared/hooks', () => ({useActions: jest.fn()}));
+jest.mock('react-redux', () => ({ useSelector: jest.fn() }));
+
+jest.mock('@/shared/hooks', () => ({ useActions: jest.fn() }));
 
 describe('edit-student-details component', () => {
+  let id;
   let historyMock;
+  let useActionsFns;
 
-  describe('loader', () => {
-    beforeEach(() => {
-      historyMock = {
-        push: jest.fn(),
-        location: {},
-        listen: jest.fn(),
-        createHref: jest.fn(),
+  const doBefore = () => {
+    useSelector
+      .mockReturnValue(mockCurrentStudentSelector)
+      .mockReturnValue(mockLoadStudentsGroupSelector)
+      .mockReturnValue(mockCurrentStudentGroupsSelector);
+
+    historyMock = {
+      push: jest.fn(),
+      location: {},
+      listen: jest.fn(),
+      createHref: jest.fn(),
+    };
+
+    useActionsFns = {
+      updateStudent: jest.fn(),
+      deleteStudent: jest.fn(),
+      dispatchAddAlert: jest.fn(),
+    };
+
+    useActions.mockReturnValue(
+      [
+        useActionsFns.updateStudent,
+        useActionsFns.deleteStudent,
+        useActionsFns.dispatchAddAlert,
+      ],
+    );
+
+    id = 1;
+  };
+
+  describe('test loader', () => {
+    beforeEach(doBefore);
+
+    it('should show loader while areStudentGroupsLoading is true', () => {
+      const mockLocalCurrentStudentSelector = {
+        ...mockCurrentStudentSelector,
+        isLoading: true,
       };
-    });
 
-    it('should show loader while isStudentLoading is true', () => {
-      const mockLocalCurrentStudentSelector = { ...mockCurrentStudentSelector, isLoading: true };
       useSelector
         .mockReturnValueOnce(mockLocalCurrentStudentSelector)
         .mockReturnValueOnce(mockLoadStudentsGroupSelector)
         .mockReturnValueOnce(mockCurrentStudentGroupsSelector);
-      const { container } = render(<Router history={historyMock}><EditStudentsDetails /></Router>);
-      // const loader = container.querySelector('.spinner-border');
-      // expect(loader).toBeInTheDocument();
-      console.log(container);
+
+      const { container } = render(
+        <Router history={historyMock}>
+          <EditStudentsDetails id={id} />
+        </Router>
+      );
+
+      const loader = container.querySelector('.spinner-border');
+      expect(loader).toBeInTheDocument();
+    });
+
+    it('should show loader while areGroupsLoading is true', () => {
+      const mockAreGroupsLoading = {
+        ...mockLoadStudentsGroupSelector,
+        isLoading: true,
+      };
+
+      useSelector
+        .mockReturnValueOnce(mockCurrentStudentSelector)
+        .mockReturnValueOnce(mockAreGroupsLoading)
+        .mockReturnValueOnce(mockCurrentStudentGroupsSelector);
+
+      const { container } = render(
+        <Router history={historyMock}>
+          <EditStudentsDetails id={id} />
+        </Router>
+      );
+      const loader = container.querySelector('.spinner-border');
+      expect(loader).toBeInTheDocument();
+    });
+
+    it('should show loader while areGroupsLoading is true', () => {
+      const mockLocalCurentStudentGroupsSelector = {
+        ...mockCurrentStudentGroupsSelector,
+        isLoading: true,
+      };
+
+      useSelector
+        .mockReturnValueOnce(mockCurrentStudentSelector)
+        .mockReturnValueOnce(mockLoadStudentsGroupSelector)
+        .mockReturnValueOnce(mockLocalCurentStudentGroupsSelector);
+
+      const { container } = render(
+        <Router history={historyMock}>
+          <EditStudentsDetails id={id} />
+        </Router>
+      );
+
+      const loader = container.querySelector('.spinner-border');
+      expect(loader).toBeInTheDocument();
     });
   });
 
-  // describe('content', () => {
+  describe('test content', () => {
+    beforeEach(doBefore);
 
-  //   it('content', () => {
-  //     render(<Router history={historyMock}><EditStudentsDetails id={id} /></Router>);
-  //     screen.debug();
-  //   });
-  // });
+    it('should render correct data in correct place', () => {
+      render(<EditStudentsDetails id={id} />);
+      screen.debug();
+    });
+  });
 });
