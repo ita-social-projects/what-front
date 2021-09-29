4d53559ee43b1f91f14614cb9b9de35d07e8284e
RomanVishnevetskyi <vrv080289@gmail.com>
2021-09-29 22:24:44 +0300

    correcting  selection row in students and group ,  change size of col

--- src/features/students/list-of-students/list-of-students.js
+++ src/features/students/list-of-students/list-of-students.js
@@ -1,19 +1,22 @@
-import React, {useEffect, useState } from 'react';
-import { useSelector, shallowEqual } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-import classNames from 'classnames';
+import React, { useEffect, useState } from "react";
+import { useSelector, shallowEqual } from "react-redux";
+import { useHistory } from "react-router-dom";
+import classNames from "classnames";
 
-import { paths, useActions } from '@/shared';
+import { paths, useActions } from "@/shared";
 import {
-  loadStudents, loadActiveStudents,
-  studentsSelector, activeStudentsSelector, currentUserSelector,
-} from '@/models';
-import { WithLoading, Pagination, Search, Button } from '@/components';
-import { addAlert } from '@/features';
-import Icon from '@/icon';
-import styles from './list-of-students.scss';
-import {List} from "@components/list";
-import {Table} from "@components/table";
+  loadStudents,
+  loadActiveStudents,
+  studentsSelector,
+  activeStudentsSelector,
+  currentUserSelector,
+} from "@/models";
+import { WithLoading, Pagination, Search, Button } from "@/components";
+import { addAlert } from "@/features";
+import Icon from "@/icon";
+import styles from "./list-of-students.scss";
+import { List } from "@components/list";
+import { Table } from "@components/table";
 
 export const ListOfStudents = () => {
   const {
@@ -30,11 +33,8 @@ export const ListOfStudents = () => {
 
   const { currentUser } = useSelector(currentUserSelector, shallowEqual);
 
-  const [
-    dispatchLoadStudents,
-    dispatchLoadActiveStudents,
-    dispatchAddAlert,
-  ] = useActions([loadStudents, loadActiveStudents, addAlert]);
+  const [dispatchLoadStudents, dispatchLoadActiveStudents, dispatchAddAlert] =
+    useActions([loadStudents, loadActiveStudents, addAlert]);
 
   const history = useHistory();
 
@@ -42,15 +42,16 @@ export const ListOfStudents = () => {
   const [visibleStudents, setVisibleStudents] = useState([]);
 
   const INITIAL_CATEGORIES = [
-    { id: 0, name: 'firstName', sortedByAscending: false, tableHead: 'Name' },
-    { id: 1, name: 'lastName', sortedByAscending: false, tableHead: 'Surname' },
-    { id: 2, name: 'email', sortedByAscending: false, tableHead: 'Email' },
+    { id: 0, name: "firstName", sortedByAscending: false, tableHead: "Name" },
+    { id: 1, name: "lastName", sortedByAscending: false, tableHead: "Surname" },
+    { id: 2, name: "email", sortedByAscending: false, tableHead: "Email" },
   ];
 
-  const [sortingCategories, setSortingCategories] = useState(INITIAL_CATEGORIES);
+  const [sortingCategories, setSortingCategories] =
+    useState(INITIAL_CATEGORIES);
   const [isShowDisabled, setIsShowDisabled] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
-  const [searchFieldValue, setSearchFieldValue] = useState('');
+  const [searchFieldValue, setSearchFieldValue] = useState("");
   const [showBlocks, setShowBlocks] = useState(false);
 
   const [studentsPerPage, setStudentsPerPage] = useState(9);
@@ -63,15 +64,18 @@ export const ListOfStudents = () => {
     return allStudents.filter(({ id }) => !activeStudentIds.includes(id));
   };
 
-  const searchStudents = (searchedStudents, value) => searchedStudents.filter(({ firstName, lastName }) => `${firstName} ${lastName}`
-    .toLowerCase().includes(value.toLowerCase()));
+  const searchStudents = (searchedStudents, value) =>
+    searchedStudents.filter(({ firstName, lastName }) =>
+      `${firstName} ${lastName}`.toLowerCase().includes(value.toLowerCase())
+    );
 
-  const changeActiveCategory = (categories, activeCategoryName) => categories.map((category) => {
-    if (category.name === activeCategoryName) {
-      return { ...category, sortedByAscending: !category.sortedByAscending };
-    }
-    return { ...category, sortedByAscending: false };
-  });
+  const changeActiveCategory = (categories, activeCategoryName) =>
+    categories.map((category) => {
+      if (category.name === activeCategoryName) {
+        return { ...category, sortedByAscending: !category.sortedByAscending };
+      }
+      return { ...category, sortedByAscending: false };
+    });
 
   const downloadStudents = () => {
     history.push(paths.STUDENTS_BY_GROUP_ID);
@@ -85,15 +89,24 @@ export const ListOfStudents = () => {
     if (isShowDisabled && allStudents.length && !areAllStudentsLoading) {
       const disabledStudents = getDisabledStudents();
 
-      setStudents(disabledStudents.map((student, index) => ({ index, ...student })));
+      setStudents(
+        disabledStudents.map((student, index) => ({ index, ...student }))
+      );
     }
     if (!isShowDisabled && activeStudents.length && !areActiveStudentsLoading) {
-      setStudents(activeStudents.map((student, index) => ({ index, ...student })));
+      setStudents(
+        activeStudents.map((student, index) => ({ index, ...student }))
+      );
     }
     setSortingCategories(INITIAL_CATEGORIES);
     setVisibleStudents(students.slice(indexOfFirstStudent, indexOfLastStudent));
-  },
-  [activeStudents, areActiveStudentsLoading, allStudents, areAllStudentsLoading, isShowDisabled]);
+  }, [
+    activeStudents,
+    areActiveStudentsLoading,
+    allStudents,
+    areAllStudentsLoading,
+    isShowDisabled,
+  ]);
 
   useEffect(() => {
     setVisibleStudents(students.slice(indexOfFirstStudent, indexOfLastStudent));
@@ -108,20 +121,29 @@ export const ListOfStudents = () => {
   useEffect(() => {
     if (isShowDisabled) {
       const disabledStudents = getDisabledStudents();
-      const searchedStudents = searchStudents(disabledStudents, searchFieldValue);
+      const searchedStudents = searchStudents(
+        disabledStudents,
+        searchFieldValue
+      );
 
-      setStudents(searchedStudents.map((student, index) => ({ index, ...student })));
+      setStudents(
+        searchedStudents.map((student, index) => ({ index, ...student }))
+      );
     } else {
       const searchedStudents = searchStudents(activeStudents, searchFieldValue);
 
-      setStudents(searchedStudents.map((student, index) => ({ index, ...student })));
+      setStudents(
+        searchedStudents.map((student, index) => ({ index, ...student }))
+      );
     }
     setCurrentPage(1);
   }, [searchFieldValue, isShowDisabled]);
 
   const handleSortByParam = (data, categoryParams) => {
     const sortedStudents = data;
-    setSortingCategories(changeActiveCategory(sortingCategories, categoryParams.sortingParam));
+    setSortingCategories(
+      changeActiveCategory(sortingCategories, categoryParams.sortingParam)
+    );
     setStudents(sortedStudents);
     setVisibleStudents(students.slice(indexOfFirstStudent, indexOfLastStudent));
   };
@@ -177,18 +199,21 @@ export const ListOfStudents = () => {
   };
 
   const listProps = {
-      data: visibleStudents,
-      handleDetails,
-      handleEdit,
-      errors: [{
-          message: 'Loading has been failed',
-          check: [!!allStudentsError, !!activeStudentsError]
-      }, {
-          message: 'Student is not found',
-          check: [!visibleStudents.length, !!searchFieldValue]
-      }],
-      access: true,
-    fieldsToShow: ['firstName', 'lastName', 'email', 'edit']
+    data: visibleStudents,
+    handleDetails,
+    handleEdit,
+    errors: [
+      {
+        message: "Loading has been failed",
+        check: [!!allStudentsError, !!activeStudentsError],
+      },
+      {
+        message: "Student is not found",
+        check: [!visibleStudents.length, !!searchFieldValue],
+      },
+    ],
+    access: true,
+    fieldsToShow: ["firstName", "lastName", "email", "edit"],
   };
 
   const paginationComponent = () => {
@@ -220,16 +245,14 @@ export const ListOfStudents = () => {
       <div className="row justify-content-between align-items-center mb-3">
         <h2 className="col-6">Students</h2>
         <div className="col-2 text-right">
-          {
-           !areActiveStudentsLoading && !areAllStudentsLoading
-          && `${visibleStudents.length} of ${students.length} students`
-        }
+          {!areActiveStudentsLoading &&
+            !areAllStudentsLoading &&
+            `${visibleStudents.length} of ${students.length} students`}
         </div>
         <div className="col-4 d-flex align-items-center justify-content-end">
-          {!areActiveStudentsLoading && !areAllStudentsLoading
-          && (
-            paginationComponent()
-          )}
+          {!areActiveStudentsLoading &&
+            !areAllStudentsLoading &&
+            paginationComponent()}
         </div>
       </div>
       <div className="row mr-0">
@@ -237,17 +260,21 @@ export const ListOfStudents = () => {
           <div className="row align-items-center d-flex justify-content-between mt-2 mb-3">
             <div className="col-2">
               <div className="btn-group">
-                <button type="button"
-                        className="btn btn-secondary"
-                        disabled={!showBlocks}
-                        onClick={() => setShowBlocks(false)}>
-                  <Icon icon="List" color="#2E3440" size={25}/>
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  disabled={!showBlocks}
+                  onClick={() => setShowBlocks(false)}
+                >
+                  <Icon icon="List" color="#2E3440" size={25} />
                 </button>
-                <button type="button"
-                        className="btn btn-secondary"
-                        disabled={showBlocks}
-                        onClick={() => setShowBlocks(true)}>
-                  <Icon icon="Card" color="#2E3440" size={25}/>
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  disabled={showBlocks}
+                  onClick={() => setShowBlocks(true)}
+                >
+                  <Icon icon="Card" color="#2E3440" size={25} />
                 </button>
               </div>
             </div>
@@ -262,29 +289,37 @@ export const ListOfStudents = () => {
               <input
                 value={isShowDisabled}
                 type="checkbox"
-                className={classNames('custom-control-input', styles['custom-control-input'])}
+                className={classNames(
+                  "custom-control-input",
+                  styles["custom-control-input"]
+                )}
                 id="show-disabled-check"
                 onChange={handleShowDisabled}
               />
               <label
-                className={classNames('custom-control-label', styles['custom-control-label'])}
+                className={classNames(
+                  "custom-control-label",
+                  styles["custom-control-label"]
+                )}
                 htmlFor="show-disabled-check"
               >
                 Disabled students
               </label>
             </div>
-            {!showBlocks &&
-              <div className="col-1 d-flex">
+            {!showBlocks && (
+              <div className="col-2 d-flex">
                 <label
-                  className={classNames(styles['label-for-select'])}
+                  className={classNames(styles["label-for-select"])}
                   htmlFor="change-visible-people"
                 >
                   Rows
                 </label>
                 <select
-                  className={classNames('form-control', styles['change-rows'])}
+                  className={classNames("form-control", styles["change-rows"])}
                   id="change-visible-people"
-                  onChange={(event) => { changeCountVisibleItems(event.target.value); }}
+                  onChange={(event) => {
+                    changeCountVisibleItems(event.target.value);
+                  }}
                 >
                   <option>9</option>
                   <option>27</option>
@@ -293,37 +328,54 @@ export const ListOfStudents = () => {
                   <option>99</option>
                 </select>
               </div>
-            }
-              {[3, 4].includes(currentUser.role) && (
-                <div className="col-4 text-right">
-                <Button onClick={downloadStudents} type="button" className={classNames('btn btn-warning ', styles['left-add-btn'])}>
+            )}
+            {[3, 4].includes(currentUser.role) && (
+              <div className="col-12 text-right pt-2">
+                <Button
+                  onClick={downloadStudents}
+                  type="button"
+                  className={classNames(
+                    "btn mr-2 btn-warning ",
+                    styles["left-add-btn"]
+                  )}
+                >
                   Upload student('s)
                 </Button>
-                <Button onClick={handleAddStudent}><span>Add a student</span></Button>
+                <Button onClick={handleAddStudent}>
+                  <span>Add a student</span>
+                </Button>
               </div>
-              )}
+            )}
           </div>
-          <WithLoading isLoading={areActiveStudentsLoading || areAllStudentsLoading} className="d-block mx-auto my-2">
-            {
-              showBlocks ?
-                  <div className="container d-flex flex-wrap">
-                    <List listType={'block'} props={listProps}/>
-                  </div>
-                  :
-                  <Table
-                      sortingCategories={sortingCategories}
-                      currentUser={currentUser}
-                      onClick={handleSortByParam}
-                      data={students}
-                      access={{unruledUser: [2], unassigned: ''}}
-                  >
-                    <List listType='list' props={listProps}/>
-                  </Table>
-            }
-
+          <WithLoading
+            isLoading={areActiveStudentsLoading || areAllStudentsLoading}
+            className="d-block mx-auto my-2"
+          >
+            {showBlocks ? (
+              <div className="container d-flex flex-wrap">
+                <List listType={"block"} props={listProps} />
+              </div>
+            ) : (
+              <Table
+                sortingCategories={sortingCategories}
+                currentUser={currentUser}
+                onClick={handleSortByParam}
+                data={students}
+                access={{ unruledUser: [2], unassigned: "" }}
+              >
+                <List listType="list" props={listProps} />
+              </Table>
+            )}
           </WithLoading>
         </div>
-        <div className={classNames('row justify-content-between align-items-center mb-3', styles.paginate)}>{paginationComponent()}</div>
+        <div
+          className={classNames(
+            "row justify-content-between align-items-center mb-3",
+            styles.paginate
+          )}
+        >
+          {paginationComponent()}
+        </div>
       </div>
     </div>
   );
