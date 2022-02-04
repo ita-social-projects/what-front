d5b9683067171f0e1f257b660370470e5b96921f
vitaliymig <vitaliy.mooving@gmail.com>
2022-02-04 12:04:51 +0200

    list-of-metnotrs and pagination components are refactored (#892)

--- src/components/pagination/pagination.js
+++ src/components/pagination/pagination.js
@@ -1,25 +1,38 @@
 import React, { useEffect, useState } from 'react';
-import propTypes from 'prop-types';
 
 import classNames from 'classnames';
 import styles from './pagination.scss';
 
-export const Pagination = ({ itemsPerPage, totalItems, paginate, prevPage, nextPage, page }) => {
-  const [currentPage, setCurrentPage] = useState(page);
+export const Pagination = ({ items = [], setVisibleItems = () => {} }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(9);
+  const totalItems = items.length;
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
 
   useEffect(() => {
-    setCurrentPage(page);
-  }, [page]);
+    setCurrentPage(1);
+  }, [items]);
+
+  useEffect(() => {
+    setVisibleItems(items.slice(indexOfFirstItem, indexOfLastItem));
+  }, [currentPage, items, itemsPerPage]);
 
   const totalPages = Math.ceil(totalItems / itemsPerPage);
   let ellipsisLeft = false;
   let ellipsisRight = false;
 
-  const pagination = [...new Array(totalPages).keys()].map((number) => number + 1)
+  const pagination = [...new Array(totalPages).keys()]
+    .map((number) => number + 1)
     .reduce((accum, i) => {
       if (i === currentPage) {
         accum.push({ id: i, current: true, ellipsis: false });
-      } else if (i < 2 || i > totalPages - 1 || i === currentPage - 1 || i === currentPage + 1) {
+      } else if (
+        i < 2 ||
+        i > totalPages - 1 ||
+        i === currentPage - 1 ||
+        i === currentPage + 1
+      ) {
         accum.push({ id: i, current: false, ellipsis: false });
       } else if (i > 1 && i < currentPage && !ellipsisLeft) {
         ellipsisLeft = true;
@@ -29,8 +42,7 @@ export const Pagination = ({ itemsPerPage, totalItems, paginate, prevPage, nextP
         accum.push({ id: i, current: false, ellipsis: true });
       }
       return accum;
-    },
-    []);
+    }, []);
 
   const changePage = (item) => {
     if (item !== currentPage) {
@@ -45,79 +57,100 @@ export const Pagination = ({ itemsPerPage, totalItems, paginate, prevPage, nextP
   const goToNextPage = () => {
     setCurrentPage((prev) => (prev === totalPages ? prev : prev + 1));
   };
+  const handleChangeItemsPerPage = (event) => {
+    setItemsPerPage(Number(event.target.value));
+  };
 
   return (
-    <nav className="d-flex flex-row flex-wrap">
-      <ul className="pagination mb-0">
-        <li className="page-item">
-          <button className={classNames("page-link", styles.link)} 
-            onClick={() => {
-              goToPrevPage();
-              prevPage(currentPage - 1);
-            }}
-          >&lt;</button>
-        </li>
-      </ul>
-      <ul className="pagination mb-0">
-        {pagination.map((page) => {
-          if (!page.ellipsis) {
-            if (totalItems === 1) {
-              return (
-                <li key={page.id} className="page-item">
-                  <button
-                    className={classNames("page-link", styles.link)}
-                    disabled
-                    onClick={() => {
-                      paginate(page.id);
-                      changePage(page.id);
-                    }}
-                  >
-                    {page.id}
-                  </button>
-                </li>
-              );
-            }
-            return (
-              <li key={page.id} className="page-item">
-                <button
-                  className={classNames("page-link", styles.link, { [styles.active]: page.current })}
-                  onClick={() => {
-                    paginate(page.id);
-                    changePage(page.id);
-                  }}
-                >
-                  {page.id}
-                </button>
-              </li>
-            );
-          } else {
-            return (
-              <li key={page.id} className="d-flex align-items-end">
-                <span className={classNames('pagination-ellipsis px-1', styles.ellipsis)}>&hellip;</span>
-              </li>
-            );
-          }
-        })}
-      </ul>
-      <ul className="pagination mb-0">
-        <li className="page-item">
-          <button
-            className={classNames("page-link", styles.link)}
-            onClick={() => {
-              goToNextPage();
-              nextPage(currentPage + 1);
-            }}
-          >&gt;</button>
-        </li>
-      </ul>
-    </nav>
+    <>
+      {totalItems > itemsPerPage ? (
+        <nav className="d-flex flex-row flex-wrap">
+          <ul className="pagination mb-0">
+            <li className="page-item">
+              <button
+                className={classNames('page-link', styles.link)}
+                onClick={() => {
+                  goToPrevPage();
+                }}
+              >
+                &lt;
+              </button>
+            </li>
+          </ul>
+          <ul className="pagination mb-0">
+            {pagination.map((page) => {
+              if (!page.ellipsis) {
+                if (totalItems === 1) {
+                  return (
+                    <li key={page.id} className="page-item">
+                      <button
+                        className={classNames('page-link', styles.link)}
+                        disabled
+                        onClick={() => {
+                          changePage(page.id);
+                        }}
+                      >
+                        {page.id}
+                      </button>
+                    </li>
+                  );
+                }
+                return (
+                  <li key={page.id} className="page-item">
+                    <button
+                      className={classNames('page-link', styles.link, {
+                        [styles.active]: page.current,
+                      })}
+                      onClick={() => {
+                        changePage(page.id);
+                      }}
+                    >
+                      {page.id}
+                    </button>
+                  </li>
+                );
+              } else {
+                return (
+                  <li key={page.id} className="d-flex align-items-end">
+                    <span
+                      className={classNames(
+                        'pagination-ellipsis px-1',
+                        styles.ellipsis
+                      )}
+                    >
+                      &hellip;
+                    </span>
+                  </li>
+                );
+              }
+            })}
+          </ul>
+          <ul className="pagination mb-0">
+            <li className="page-item">
+              <button
+                className={classNames('page-link', styles.link)}
+                onClick={() => {
+                  goToNextPage();
+                }}
+              >
+                &gt;
+              </button>
+            </li>
+          </ul>
+        </nav>
+      ) : (
+        <></>
+      )}
+      <label className="m-0 mr-3 d-flex align-items-center">
+        <span className="mr-3">Rows</span>
+        <select className="form-control" onChange={handleChangeItemsPerPage}>
+          <option>9</option>
+          <option>27</option>
+          <option>45</option>
+          <option>72</option>
+          <option>99</option>
+        </select>
+      </label>
+    </>
   );
-};
-
-Pagination.propTypes = {
-  itemsPerPage: propTypes.number.isRequired,
-  totalItems: propTypes.number.isRequired,
-  paginate: propTypes.func.isRequired,
-  prevPage: propTypes.func.isRequired,
-  nextPage: propTypes.func.isRequired,
 };
