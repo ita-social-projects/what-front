a73c801cba797dad618d7a42b4df1ba8f1501c85
Naira Kazeian <nkazeian@gmail.com>
2022-02-17 23:36:21 +0200

    removed extra component

--- src/screens/students/students-tabs.js
+++ src/screens/students/students-tabs.js
@@ -1,9 +1,13 @@
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useActions } from '@/shared';
-import { globalLoadStudentGroups, loadStudentById, loadStudentGroups, fetchLessonsByStudentId } from '@/models';
-import { Tab, Tabs } from '@/components';
-import { EditStudentsDetails, StudentDetails } from '@/features/students';
+import {
+  globalLoadStudentGroups,
+  loadStudentById,
+  loadStudentGroups,
+  fetchLessonsByStudentId,
+} from '@/models';
+import { Link } from 'react-router-dom';
 
 export const StudentsTabs = ({ index }) => {
   const { id } = useParams();
@@ -25,16 +29,45 @@ export const StudentsTabs = ({ index }) => {
     fetchGroups();
     fetchStudentGroups(id);
     fetchStudentLessons(id);
-  }, [fetchGroups, fetchStudentById, fetchStudentGroups, fetchStudentLessons, id]);
+  }, [
+    fetchGroups,
+    fetchStudentById,
+    fetchStudentGroups,
+    fetchStudentLessons,
+    id,
+  ]);
+
+  const arrow = (
+    <svg
+      width="1em"
+      height="1em"
+      viewBox="0 0 16 16"
+      className="bi bi-arrow-left"
+      fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
+      />
+    </svg>
+  );
 
   return (
-    <Tabs defaultIndex={index} className="container w-50 pt-5" linkBack="/students">
-      <Tab title="Student details">
-        <StudentDetails />
-      </Tab>
-      <Tab title="Edit student details">
-        <EditStudentsDetails id={id} />
-      </Tab>
-    </Tabs>
+    <section
+      defaultIndex={index}
+      className="container w-50 pt-5"
+      linkBack="/students"
+    >
+      <Link
+        className="nav-item nav-link d-flex align-items-center"
+        to={{
+          pathname: '/students',
+        }}
+      >
+        {arrow}
+      </Link>
+      <StudentDetails />
+    </section>
   );
 };
