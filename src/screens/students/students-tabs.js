4e206ab0b4b25330124ec5f779b1908aaab22120
Naira21 <79223794+Naira21@users.noreply.github.com>
2022-02-22 16:14:46 +0200

    Kazeian 875 students table save (#918)
    * removed extra component
    
    * return student details
    
    * add condition for editing component

--- src/screens/students/students-tabs.js
+++ src/screens/students/students-tabs.js
@@ -1,11 +1,21 @@
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useActions } from '@/shared';
-import { globalLoadStudentGroups, loadStudentById, loadStudentGroups, fetchLessonsByStudentId } from '@/models';
+import {
+  globalLoadStudentGroups,
+  loadStudentById,
+  loadStudentGroups,
+  fetchLessonsByStudentId,
+} from '@/models';
+import { Link } from 'react-router-dom';
 import { Tab, Tabs } from '@/components';
-import { EditStudentsDetails, StudentDetails } from '@/features/students';
+import { StudentDetails, EditStudentsDetails } from '@/features/students';
+
+import { shallowEqual, useSelector } from 'react-redux';
+import { currentUserSelector } from '@/models';
 
 export const StudentsTabs = ({ index }) => {
+  const { currentUser } = useSelector(currentUserSelector, shallowEqual);
   const { id } = useParams();
 
   const [
@@ -25,16 +35,62 @@ export const StudentsTabs = ({ index }) => {
     fetchGroups();
     fetchStudentGroups(id);
     fetchStudentLessons(id);
-  }, [fetchGroups, fetchStudentById, fetchStudentGroups, fetchStudentLessons, id]);
+  }, [
+    fetchGroups,
+    fetchStudentById,
+    fetchStudentGroups,
+    fetchStudentLessons,
+    id,
+  ]);
+
+  const arrow = (
+    <svg
+      width="1em"
+      height="1em"
+      viewBox="0 0 16 16"
+      className="bi bi-arrow-left"
+      fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
+      />
+    </svg>
+  );
 
   return (
-    <Tabs defaultIndex={index} className="container w-50 pt-5" linkBack="/students">
-      <Tab title="Student details">
-        <StudentDetails />
-      </Tab>
-      <Tab title="Edit student details">
-        <EditStudentsDetails id={id} />
-      </Tab>
-    </Tabs>
+    <>
+      {currentUser.role === 2 ? (
+        <section
+          defaultIndex={index}
+          className="container w-50 pt-5"
+          linkBack="/students"
+        >
+          <Link
+            className="nav-item nav-link d-flex align-items-center"
+            to={{
+              pathname: '/students',
+            }}
+          >
+            {arrow}
+          </Link>
+          <StudentDetails />
+        </section>
+      ) : (
+        <Tabs
+          defaultIndex={index}
+          className="container w-50 pt-5"
+          linkBack="/students"
+        >
+          <Tab title="Student details">
+            <StudentDetails />
+          </Tab>
+          <Tab title="Edit student details">
+            <EditStudentsDetails id={id} />
+          </Tab>
+        </Tabs>
+      )}
+    </>
   );
 };
