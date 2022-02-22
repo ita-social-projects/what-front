c17576c140d0d932b1ce1822092b76c8625b4d49
Anna Samsonenko <foster.anna93@gmail.com>
2022-02-22 15:28:34 +0200

    refactors list of courses

--- src/features/courses/list-of-courses/list-of-courses.js
+++ src/features/courses/list-of-courses/list-of-courses.js
@@ -1,7 +1,9 @@
 import React, { useCallback, useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
-import { useLocation } from 'react-router';
 import { shallowEqual, useSelector } from 'react-redux';
+import classNames from 'classnames';
+import { List } from '@components/list';
+import { Table } from '@components/table';
 import { useActions, paths } from '@/shared';
 import {
   fetchActiveCourses,
@@ -14,129 +16,75 @@ import { Button, Search, WithLoading, Pagination } from '@/components/index.js';
 
 import Icon from '@/icon.js';
 
-import classNames from 'classnames';
 import styles from './list-of-courses.scss';
-import { List } from '@components/list';
-import { Table } from '@components/table';
 
 export const ListOfCourses = () => {
   const history = useHistory();
-  const pagPage = useLocation();
-  const paginationPage = pagPage.state
-    ? pagPage.state.paginationPage.paginationPage
-    : 1;
 
+  // this is needed for future work with redirect by arrow button
+  // const location = useLocation();
+  // const paginationPage = location?.state
+  //   ? location?.state?.paginationPage?.currentPage
+  //   : 1;
+
+  const [isShowDisabled, setIsShowDisabled] = useState(false);
+  const [filteredCourses, setFilteredCourses] = useState([]);
   const [visibleCourses, setVisibleCourses] = useState([]);
-
-  const [coursesPerPage, setcoursesPerPage] = useState(9);
-  const [currentPage, setCurrentPage] = useState(paginationPage);
-
   const [searchValue, setSearchValue] = useState('');
   const [showBlocks, setShowBlocks] = useState(false);
-
-  const [filteredCourses, setFilteredCourses] = useState([]);
-
   const [sortingCategories, setSortingCategories] = useState([
     { id: 0, name: 'name', sortedByAscending: false, tableHead: 'Title' },
   ]);
 
-  const { data: activeCourses, isLoading: areActiveCoursesLoading } =
-    useSelector(coursesActiveSelector, shallowEqual);
+  const { currentUser } = useSelector(currentUserSelector, shallowEqual);
+  const {
+    data: activeCourses,
+    isLoading: areActiveCoursesLoading,
+  } = useSelector(coursesActiveSelector, shallowEqual);
 
-  const { data: notActiveCourses, isLoading: areNotActiveCoursesLoading } =
-    useSelector(coursesNotActiveSelector, shallowEqual);
-
-  const { currentUser } = useSelector(currentUserSelector, shallowEqual); // role of user
+  const {
+    data: notActiveCourses,
+    isLoading: areNotActiveCoursesLoading,
+  } = useSelector(coursesNotActiveSelector, shallowEqual);
 
   const [loadActiveCourses, loadNotActiveCourses] = useActions([
     fetchActiveCourses,
     fetchNotActiveCourses,
-  ]); // loading courses
-
-  const [courses, setCourses] = useState([]);
-
-  const [isShowDisabled, setIsShowDisabled] = useState(false);
-
-  const indexOfLastCourse = currentPage * coursesPerPage;
-  const indexOfFirstCourse = indexOfLastCourse - coursesPerPage;
-
-  useEffect(() => {
-    loadActiveCourses();
-  }, [loadActiveCourses]);
-
-  useEffect(() => {
-    loadNotActiveCourses();
-  }, [loadNotActiveCourses]);
-
-  useEffect(() => {
-    if (
-      isShowDisabled &&
-      notActiveCourses.length &&
-      !areNotActiveCoursesLoading
-    ) {
-      setCourses(notActiveCourses);
-    }
-    if (!isShowDisabled && activeCourses.length && !areActiveCoursesLoading) {
-      setCourses(activeCourses);
-    }
-    setVisibleCourses(courses.slice(indexOfFirstCourse, indexOfLastCourse));
-  }, [
-    activeCourses,
-    areActiveCoursesLoading,
-    notActiveCourses,
-    areNotActiveCoursesLoading,
-    isShowDisabled,
   ]);
 
   useEffect(() => {
-    setFilteredCourses(courses);
-  }, [courses]);
+    if (!isShowDisabled) {
+      loadActiveCourses();
+    }
+  }, [isShowDisabled, loadActiveCourses]);
 
   useEffect(() => {
-    setVisibleCourses(
-      filteredCourses.slice(indexOfFirstCourse, indexOfLastCourse)
-    );
-  }, [currentPage, filteredCourses]);
-
-  const coursesList = () => {
-    const courses = visibleCourses.map((course) => (
-      <tr
-        key={course.id}
-        onClick={() => courseDetails(course.id)}
-        className={styles['table-row']}
-        data-student-id={course.id}
-      >
-        <td className="text-left">{course.name}</td>
-        {(currentUser.role === 8 || currentUser.role === 4) && (
-          <td
-            className="text-center"
-            onClick={(event) => courseEdit(event, course.id)}
-            data-student-id={course.id}
-          >
-            <Icon
-              icon="Edit"
-              className={styles.scale}
-              color="#2E3440"
-              size={30}
-            />
-          </td>
-        )}
-      </tr>
-    ));
-
-    if (!courses.length && searchValue) {
-      return <h4>Course is not found</h4>;
+    if (activeCourses.length > 0) {
+      setFilteredCourses(activeCourses);
     }
-    return courses;
-  };
+  }, [activeCourses]);
+
+  useEffect(() => {
+    if (notActiveCourses.length > 0) {
+      setFilteredCourses(notActiveCourses);
+    }
+  }, [notActiveCourses]);
 
   const handleSearch = (inputValue) => {
     setSearchValue(inputValue);
-    setVisibleCourses(
-      activeCourses.filter(({ name }) =>
-        name.toLowerCase().includes(inputValue.toLowerCase())
-      )
-    );
+    if (!isShowDisabled) {
+      setFilteredCourses(
+        activeCourses.filter(({ name }) =>
+          name.toLowerCase().includes(inputValue.toLowerCase())
+        )
+      );
+    } else {
+      setFilteredCourses(
+        notActiveCourses.filter(({ name }) =>
+          name.toLowerCase().includes(inputValue.toLowerCase())
+        )
+      );
+    }
   };
 
   const changeActiveCategory = (categories, activeCategoryName) =>
@@ -155,10 +103,10 @@ export const ListOfCourses = () => {
     (id) => {
       history.push({
         pathname: `${paths.COURSE_DETAILS}/${id}`,
-        state: { currentPage },
+        // state: { currentPage },
       });
     },
-    [history, currentPage]
+    [history]
   );
 
   const handleEdit = useCallback(
@@ -166,10 +114,10 @@ export const ListOfCourses = () => {
       event.stopPropagation();
       history.push({
         pathname: `${paths.COURSE_EDIT}/${id}`,
-        state: { currentPage },
+        // state: { currentPage },
       });
     },
-    [history, currentPage]
+    [history]
   );
 
   const handleSortByParam = (data, categoryParams) => {
@@ -178,66 +126,12 @@ export const ListOfCourses = () => {
       changeActiveCategory(sortingCategories, categoryParams.sortingParam)
     );
     setFilteredCourses(sortedCourses);
-    setVisibleCourses(
-      sortedCourses.slice(indexOfFirstCourse, indexOfLastCourse)
-    );
   };
 
   const handleShowDisabled = (event) => {
     setIsShowDisabled(!isShowDisabled);
     if (event.target.checked) {
       loadNotActiveCourses();
-    } else {
-      loadActiveCourses();
-    }
-  };
-
-  const paginate = (pageNumber) => {
-    if (currentPage !== pageNumber) {
-      setCurrentPage(pageNumber);
-    }
-  };
-
-  const nextPage = (pageNumber) => {
-    const totalPages = Math.ceil(filteredCourses.length / 10);
-    setCurrentPage((prev) => {
-      if (prev === totalPages) {
-        return prev;
-      }
-      return pageNumber;
-    });
-  };
-
-  const prevPage = (pageNumber) => {
-    setCurrentPage((prev) => {
-      if (prev - 1 === 0) {
-        return prev;
-      }
-      return pageNumber;
-    });
-  };
-
-  const changeCountVisibleItems = (newNumber) => {
-    const finish = currentPage * newNumber;
-    const start = finish - newNumber;
-    const visibleCoursesList = newNumber > data.length ? data : data.slice(start, finish)
-    
-    setVisibleCourses(visibleCoursesList);
-    setcoursesPerPage(newNumber);
-  };
-
-  const paginationComponent = () => {
-    if (activeCourses.length > coursesPerPage) {
-      return (
-        <Pagination
-          itemsPerPage={coursesPerPage}
-          totalItems={courses.length}
-          paginate={paginate}
-          prevPage={prevPage}
-          nextPage={nextPage}
-          page={currentPage}
-        />
-      );
     }
   };
 
@@ -259,16 +153,15 @@ export const ListOfCourses = () => {
     <div className="container pt-5">
       <div className="row justify-content-between align-items-center mb-3">
         <h2 className="col-6">Courses</h2>
-        <span className="col-2 text-right">
-          {visibleCourses.length} of {filteredCourses.length} courses
-        </span>
-        <div className="col-4 d-flex align-items-center justify-content-end">
-          {paginationComponent()}
-        </div>
+        {filteredCourses.length > 0 && (
+          <span className="col-2 text-right">
+            {visibleCourses.length} of {filteredCourses.length} courses
+          </span>
+        )}
       </div>
       <div className="row mr-0">
         <div className="col-12 card shadow p-3 mb-5 bg-white ml-2 mr-2">
-          <div className="row align-items-center mt-2 mb-3">
+          <div className="row align-items-center justify-content-between mt-2 mb-3">
             <div className="col-2">
               <div className="btn-group">
                 <button
@@ -313,29 +206,6 @@ export const ListOfCourses = () => {
                 Disabled Courses
               </label>
             </div>
-            {!showBlocks && (
-              <div className="col-2 d-flex">
-                <label
-                  className={classNames(styles['label-for-select'])}
-                  htmlFor="change-visible-people"
-                >
-                  Rows
-                </label>
-                <select
-                  className={classNames('form-control', styles['change-rows'])}
-                  id="change-visible-people"
-                  onChange={(event) => {
-                    changeCountVisibleItems(event.target.value);
-                  }}
-                >
-                  <option>9</option>
-                  <option>27</option>
-                  <option>45</option>
-                  <option>72</option>
-                  <option>99</option>
-                </select>
-              </div>
-            )}
             <div className="col-2 text-right">
               {[8, 4].includes(currentUser.role) && (
                 <Button onClick={addCourse}>
@@ -353,14 +223,14 @@ export const ListOfCourses = () => {
                 className="container d-flex flex-wrap"
                 data-testid="cardBlocks"
               >
-                <List listType={'block'} props={listProps} />
+                <List listType="block" props={listProps} />
               </div>
             ) : (
               <Table
                 sortingCategories={sortingCategories}
                 currentUser={currentUser}
                 onClick={handleSortByParam}
-                data={filteredCourses}
+                data={isShowDisabled ? notActiveCourses : activeCourses}
                 access={{ unruledUser: [1, 2], unassigned: '' }}
               >
                 <List listType="list" props={listProps} />
@@ -374,7 +244,12 @@ export const ListOfCourses = () => {
             styles.paginate
           )}
         >
-          {paginationComponent()}
+          {filteredCourses.length > 0 && (
+            <Pagination
+              items={filteredCourses}
+              setVisibleItems={setVisibleCourses}
+            />
+          )}
         </div>
       </div>
     </div>
